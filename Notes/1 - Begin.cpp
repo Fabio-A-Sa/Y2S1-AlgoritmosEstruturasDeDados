43b76ae3b09bde38049685877885d2f2e3187a92
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-10-24 15:30:46 +0100

    Test

--- Notes/1 - Begin.cpp
+++ Notes/1 - Begin.cpp
@@ -6,3 +6,13 @@ int main () {
     cout << "Hello world!" << endl;
     return 0;
 }
+
+long sum (int n)
+    long sum = 0;
+    int k = 1;
+    while (k <= n) {
+        sum += k;
+        k++;
+    }
+    return sum;
+}
