07ddf643e333895ecdb7e3becf02c9cf132cb4db
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-10-24 18:43:05 +0100

    Changes

--- Exercises/Aula 1/Ex2 - Mail/mail.h
+++ Exercises/Aula 1/Ex2 - Mail/mail.h
@@ -5,29 +5,33 @@
 using namespace std;
 
 class Mail {
+
 	string sender;
 	string receiver;
 	string zipCode;
-public:
-	Mail(string send, string rec, string zcode);
-	virtual ~Mail();
-	string getZipCode() const;
-	unsigned int getPrice() const;
+
+    public:
+        Mail(string send, string rec, string zcode);
+        virtual ~Mail();
+        string getZipCode() const;
+        unsigned int getPrice() const;
 };
 
 
 class RegularMail: public Mail {
+
 	unsigned int weight;
-public:
-	RegularMail(string send, string rec, string code, unsigned int w);
+
+    public:
+        RegularMail(string send, string rec, string code, unsigned int w);
 };
 
 
 class GreenMail: public Mail {
-	string type;  // "envelope", "bag", "box"
-public:
-	GreenMail(string send, string rec, string code, string t);
-};
 
+	string type;  // "envelope", "bag", "box"
+    public:
+        GreenMail(string send, string rec, string code, string t);
+};
 
 #endif /* SRC_MAIL_H_ */
