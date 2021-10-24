ea9d0fe4ef2b9c53cc036a31b1ba93e1b49f56f2
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-10-24 17:57:21 +0100

    Exercise 1, Lesson 1

--- Exercises/Aula 1/Ex1 - Parque/Test.cpp
+++ Exercises/Aula 1/Ex1 - Parque/Test.cpp
@@ -42,54 +42,44 @@ int main () {
     cout << "Testa se não deixa entrar quando a lotação está completa: ";
     answer = !p3.entrar("Rui") ? "Correct" : "Error" ; cout << answer << endl;
 
+    ParqueEstacionamento p4(3, 5);
+    p4.adicionaCliente("Joao");
+    p4.adicionaCliente("Maria");
+    p4.adicionaCliente("Antonio");
+    p4.entrar("Maria");
+    cout << "Testa se não deixa remover cliente que está dentro do parque: ";
+    answer = !p4.retiraCliente("Maria") ? "Correct" : "Error" ; cout << answer << endl;
+    cout << "Testa se deixa remover cliente que está fora do parque: ";
+    answer = p4.retiraCliente("Antonio") ? "Correct" : "Error" ; cout << answer << endl;
+    cout << "Testa se não deixa remover cliente que não existe: ";
+    answer = !p4.retiraCliente("Ana") ? "Correct" : "Error" ; cout << answer << endl;
+
+    ParqueEstacionamento p5(3, 5);
+    p5.adicionaCliente("Joao");
+    p5.adicionaCliente("Maria");
+    p5.adicionaCliente("Rui");
+    p5.entrar("Maria");
+    p5.entrar("Joao");
+    p5.entrar("Rui");
+    cout << "Testa se um cliente estacionado no parque, sai: ";
+    answer = p5.sair("Maria") ? "Correct" : "Error" ; cout << answer << endl;
+    cout << "Testa se não deixa sair um cliente que não está estacionado: ";
+    answer = !p5.sair("Maria") ? "Correct" : "Error" ; cout << answer << endl;
+    cout << "Testa se não deixa sair um cliente que não existe: ";
+    answer = !p5.sair("Antonio") ? "Correct" : "Error" ; cout << answer << endl;
+
+    ParqueEstacionamento p6(3, 5);
+    p6.adicionaCliente("Joao");
+    p6.adicionaCliente("Maria");
+    p6.adicionaCliente("Antonio");
+    p6.entrar("Maria");
+    p6.entrar("Antonio");
+    cout << "Testa se lotação do parque está correcta: ";
+    answer = p6.getNumLugares() == 3 ? "Correct" : "Error" ; cout << answer << endl;
+    cout << "Testa se o número de viaturas presentes no parque está correcto: ";
+    answer = p6.getNumLugaresOcupados() == 2 ? "Correct" : "Error" ; cout << answer << endl;
+    cout << "Testa se o número de clientes registados no parque está correcto: ";
+    answer = p6.getNumClientesAtuais()  == 3 ? "Correct" : "Error" ; cout << answer << endl;
 
     return 0;
 }
-
-/*
-
-TEST(test_1, retirarCliente){
-    ParqueEstacionamento p1(3, 5);
-    p1.adicionaCliente("Joao");
-    p1.adicionaCliente("Maria");
-    p1.adicionaCliente("Antonio");
-    p1.entrar("Maria");
-    // Testa se nao deixa remover cliente que esta dentro do parque
-    ASSERT_EQ(false, p1.retiraCliente("Maria"));
-    // Testa se deixa remover cliente que esta fora do parque
-    ASSERT_EQ(true, p1.retiraCliente("Antonio"));
-    // Testa se nao deixa remover cliente que nao existe
-    ASSERT_EQ(false, p1.retiraCliente("Ana"));
-}
-
-TEST(test_1, sairParque){
-    ParqueEstacionamento p1(3, 5);
-    p1.adicionaCliente("Joao");
-    p1.adicionaCliente("Maria");
-    p1.adicionaCliente("Rui");
-    p1.entrar("Maria");
-    p1.entrar("Joao");
-    p1.entrar("Rui");
-    // Testa se um cliente estacionado no parque, sai.
-    ASSERT_EQ(true, p1.sair("Maria"));
-    // Testa se nao deixa sair um cliente que nao esta estacionado.
-    ASSERT_EQ(false, p1.sair("Maria"));
-    // Testa se nao deixa sair um cliente que nao existe.
-    ASSERT_EQ(false, p1.sair("Antonio"));
-}
-
-TEST(test_1, lugaresLotacaoParque){
-    ParqueEstacionamento p1(3, 5);
-    p1.adicionaCliente("Joao");
-    p1.adicionaCliente("Maria");
-    p1.adicionaCliente("Antonio");
-    p1.entrar("Maria");
-    p1.entrar("Antonio");
-    // Testa se Lotacao do parque esta correcta
-    ASSERT_EQ(3, p1.getNumLugares());
-    // Testa se o numero de viaturas presentes no parque esta correcto
-    ASSERT_EQ(2, p1.getNumLugaresOcupados());
-    // Testa se o o numero de clientes registados no parque esta correcto
-    ASSERT_EQ(3, p1.getNumClientesAtuais());
-}
-*/
