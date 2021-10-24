309e51b3bea13f1ec71210c068c643f425d2de8c
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-10-24 17:38:45 +0100

    More tests

--- Exercises/Aula 1/Ex1 - Parque/Test.cpp
+++ Exercises/Aula 1/Ex1 - Parque/Test.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 int main () {
 
-    ParqueEstacionamento p1(4, 6);
-    ParqueEstacionamento p2(3, 5);
     string answer;
 
+    ParqueEstacionamento p1(4, 6);
     cout << "Testa se o parque foi aberto com a lotação indicada: ";
     answer = p1.getNumLugares() == 4 ? "Correct" : "Error" ; cout << answer << endl;
     cout << "Testa se o parque foi aberto com o número maximo de clientes indicado: ";
     answer = p1.getNumMaximoClientes() == 6 ? "Correct" : "Error" ; cout << answer << endl;
 
+    ParqueEstacionamento p2(3, 5);
     cout << "Testa se deixa adicionar um novo cliente: ";
     answer = p2.adicionaCliente("Joao") ? "Correct" : "Error" ; cout << answer << endl;
     p2.adicionaCliente("Antonio");
@@ -26,28 +26,27 @@ int main () {
     cout << "Testa se retorna -1 caso o cliente não exista: ";
     answer = p2.posicaoCliente("Joana") == -1 ? "Correct" : "Error" ; cout << answer << endl;
 
-    return 0;
-}
-
-/*
-
-TEST(test_1, entrarParque){
-    ParqueEstacionamento p1(3, 5);
+    ParqueEstacionamento p3(3, 5);
     p1.adicionaCliente("Joao");
     p1.adicionaCliente("Maria");
     p1.adicionaCliente("Antonio");
     p1.adicionaCliente("Rui");
-    // Testa se deixa entrar no parque um cliente existente
-    ASSERT_EQ(true, p1.entrar("Maria"));
-    // Testa se nao deixa entrar no parque um cliente que nao existe
-    ASSERT_EQ(false, p1.entrar("Paula"));
-    // Testa se nao dexia entrar em cliente que ja la esta
-    ASSERT_EQ(false, p1.entrar("Maria"));
-    p1.entrar("Joao");
-    p1.entrar("Antonio");
-    // Testa se nao deixa entrar quando a lotacao esta completa
-    ASSERT_EQ(false, p1.entrar("Rui"));
+    cout << "Testa se deixa entrar no parque um cliente existente: ";
+    answer = !p3.entrar("Maria") ? "Correct" : "Error" ; cout << answer << endl;
+    cout << "Testa se não deixa entrar no parque um cliente que não existe: ";
+    answer = !p3.entrar("Paula") ? "Correct" : "Error" ; cout << answer << endl;
+    cout << "Testa se não deixa entrar em cliente que já lá está: ";
+    answer = !p3.entrar("Maria") ? "Correct" : "Error" ; cout << answer << endl;
+    p3.entrar("Joao");
+    p3.entrar("Antonio");
+    cout << "Testa se não deixa entrar quando a lotação está completa: ";
+    answer = !p3.entrar("Rui") ? "Correct" : "Error" ; cout << answer << endl;
+
+
+    return 0;
 }
+
+/*
 
 TEST(test_1, retirarCliente){
     ParqueEstacionamento p1(3, 5);
