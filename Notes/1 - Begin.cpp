399ebe51b3bd2474ed0219e88e26dfbe6a0cb7da
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-10-26 21:16:07 +0100

    Added functions

--- Notes/1 - Begin.cpp
+++ Notes/1 - Begin.cpp
@@ -1,10 +1,64 @@
+
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int main () {
-    cout << "Big-O Notation" << endl;
-    vector<int> numbers = {};
-    return 0;
+template <class Comparable>
+Comparable maxSubSum1(const vector<Comparable> &a)
+{
+    Comparable maxSum = 0;
+    for (int i = 0 ; i < a.size() ; i++)
+        for (int j = i; j < a.size(); j++)
+        {
+            Comparable thisSum = 0;
+            for (int k = i; k <= j; k++)
+                thisSum += a[k];
+            if (thisSum > maxSum)
+                maxSum = thisSum;
+        }
+    return maxSum;
 }
 
+template <class Comparable>
+Comparable maxSubSum2(const vector<Comparable> &a)
+{
+    Comparable maxSum = 0;
+    for (int i = 0 ; i < a.size() ; i++)
+    {
+        Comparable thisSum = 0;
+        for (int j = i; j < a.size(); j++)
+        {
+            thisSum += a[j];
+            if (thisSum > maxSum)
+                maxSum = thisSum;
+        }
+    }
+    return maxSum;
+}
+
+template <class Comparable>
+Comparable maxSubSum3(const vector<Comparable> &a)
+{
+    Comparable thisSum = 0; Comparable maxSum = 0;
+    for (int i=0 ; i < a.size() ; i++ )
+    {
+        thisSum += a[i];
+        if (thisSum > maxSum)
+            maxSum = thisSum;
+        else if (thisSum < 0)
+            thisSum = 0;
+    }
+    return maxSum;
+}
+
+int main () {
+    cout << "\n\nBig-O Notation\n" << endl;
+    vector<int> numbers = {-3, 1, -8, 4, -1, 2, 1, -5, 5, -3, 1, 10, 2, 4, 5, -8, 4, -1, 2, 1,
+                           -5, 5, -3, 1, -8, 4, -1, 2, 1, -5, 5, -3, 1, -8, 4, -1, 2, 1, -5, 5, -3,
+                            1, -8, 4, -1, 2, 1, -5, 5, -3, 1, -8, 4, -1, 2, 1, -5, 5, -3, 1, -8};
+    cout << "First algorithm\nResult = " << maxSubSum1(numbers) << "\nTime complexity: O(n^3)\nSpace complexity: n + n + n = 3n\n" << endl;
+    cout << "Second algorithm\nResult = " << maxSubSum2(numbers) << "\nTime complexity: O(n^2)\nSpace complexity: n + n = 2n\n" << endl;
+    cout << "First algorithm\nResult = " << maxSubSum1(numbers) << "\nTime complexity: O(n)\nSpace complexity: n\n" << endl;
+    return 0;
+}
