39dfe4efe4fb83473d687381eb3d9120e18c9cfd
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-10-26 22:59:22 +0100

    algorithms#

--- Notes/1 - Correctness Analysis.cpp
+++ Notes/1 - Correctness Analysis.cpp
@@ -17,9 +17,13 @@ int main () {
     double result = squareRoot(25.651);
     cout << "\n\nPreconditions and postconditions\n" << endl;
     cout << "X = " << x << endl << "Result = squareRoot(X) = " << result << endl;
-    cout << "Preconditions: x >= 0\nPostconditions: result >= 0 && pow(result, 2) == x" << endl;
+    cout << "Preconditions: x >= 0\nPostconditions: result >= 0 && pow(result, 2) == x\n" << endl;
 
-    cout << "Parcial correctness:\n\t- Com as pré-condições correctas, caso o algoritmo termine então está correcto;\n\t- Detetar a invariante do ciclo e provar a veracidade para o início, meio e fim\n\n"
+    cout << "Parcial correctness:\n\t- Com as pré-condições correctas, caso o algoritmo termine então está correcto;" << endl;
+    cout << "\t- Detetar a invariante do ciclo e provar a veracidade para o início, meio e fim;\n" << endl;
+    cout << "Total correctness:\n\t- Com as pré-condições correctas provar que o algoritmo termina;" << endl;
+    cout << "\t- Detetar a invariante do ciclo e provar a veracidade para o início, meio e fim;" << endl;
+    cout << "\t- Detetar a variante do ciclo e provar que é uma expressão inteira, não-negativa e decrescente;\n\n" << endl;
 
     return 0;
 }
