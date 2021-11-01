966fb27f4ee5370713d2131d8d74324015a16a8d
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-01 14:48:46 +0000

    Exercise 1.2#

---
+++ Exercises/Aula 1/Ex2 - Mail/mail.cpp
@@ -0,0 +1,38 @@
+#include "mail.h"
+
+Mail::Mail(string send, string rec, string zcode) {
+    this->sender = send;
+    this->zipCode = zcode;
+    this->receiver = rec;
+}
+
+Mail::~Mail() {};
+
+string Mail::getZipCode() const {
+	return zipCode;
+}
+
+RegularMail::RegularMail(string send, string rec, string zcode, unsigned int w) : Mail(send, rec, zcode) {
+    this->weight = w;
+}
+
+GreenMail::GreenMail(string send, string rec, string zcode, string t) : Mail(send, rec, zcode) {
+    this->type = t;
+}
+
+unsigned int Mail::getPrice() const {
+    return 0;
+}
+
+unsigned int RegularMail::getPrice() const {
+	if (weight <= 20) return 50;
+    if (weight > 20 && weight <= 100) return 75;
+    if (weight > 100 && weight <= 500) return 140;
+    return 325;
+}
+
+unsigned int GreenMail::getPrice() const {
+    if (type == "envelope") return 80;
+    if (type == "bag") return 200;
+    return 240;
+}
---
+++ Exercises/Aula 1/Ex2 - Mail/postOffice.cpp
@@ -0,0 +1,97 @@
+#include "postOffice.h"
+#include <string>
+#include <sstream>
+
+PostOffice::PostOffice() {}
+
+PostOffice::PostOffice(string firstZCode, string lastZCode) {
+    this->firstZipCode = firstZCode;
+    this->lastZipCode = lastZCode;
+}
+
+NoPostmanException::NoPostmanException(string name) {
+    this->name = name;
+}
+
+string NoPostmanException::getName() const {
+    return name;
+}
+
+void PostOffice::addMailToSend(Mail *m) {
+	mailToSend.push_back(m);
+}
+
+void PostOffice::addMailToDeliver(Mail *m) {
+	mailToDeliver.push_back(m);
+}
+
+void PostOffice::addPostman(const Postman &p){
+	postmen.push_back(p);
+}
+
+vector<Mail *> PostOffice::getMailToSend() const {
+	return mailToSend;
+}
+
+vector<Mail *> PostOffice::getMailToDeliver() const {
+	return mailToDeliver;
+}
+
+vector<Postman> PostOffice::getPostman() const {
+	return postmen;
+}
+
+vector<Mail *> PostOffice::removePostman(string name) {
+
+	vector<Mail *> res = {};
+    for (vector<Postman>::iterator p = postmen.begin() ; p != postmen.end() ; p++) {
+        if ((*p).getName() == name) {
+            res = (*p).getMail();
+            postmen.erase(p);
+            return res;
+        }
+    }
+	return res;
+}
+
+vector<Mail *> PostOffice::endOfDay(unsigned int &balance) {
+
+	unsigned int total = 0;
+    vector<Mail *> answer;
+    string n1 = firstZipCode.substr(0, 4) + firstZipCode.substr(5, 3);
+    stringstream toInt1(n1);
+    int minimo = 0; toInt1 >> minimo;
+    string n2 = lastZipCode.substr(0, 4) + lastZipCode.substr(5, 3);
+    stringstream toInt2(n2);
+    int maximo = 0; toInt2 >> maximo;
+
+    for (vector<Mail *>::iterator i = mailToSend.begin() ; i != mailToSend.end() ; i++) {
+
+        total += (*i)->getPrice();
+
+        string currentZip = (*i)->getZipCode().substr(0, 4) + (*i)->getZipCode().substr(5, 3);
+        stringstream toInt(currentZip);
+        int current = 0; toInt >> current;
+
+        if (current >= minimo && current <= maximo) {
+            addMailToDeliver(*i);
+        } else {
+            answer.push_back(*i);
+        }
+    }
+    mailToSend.clear();
+    balance = total;
+	return answer;
+}
+
+Postman PostOffice::addMailToPostman(Mail *m, string name) {
+
+    for (auto postman : postmen) {
+        if (postman.getName() == name) {
+            postman.addMail(m);
+            return postman;
+        }
+    }
+    NoPostmanException non = NoPostmanException(name);
+    throw (non);
+}
