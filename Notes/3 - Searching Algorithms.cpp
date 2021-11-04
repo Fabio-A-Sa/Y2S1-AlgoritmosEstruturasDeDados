0afd7b808e72f811dd26240a86f1496d7b5258e0
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-04 21:11:10 +0000

    Binary Search

--- Notes/3 - Searching Algorithms.cpp
+++ Notes/3 - Searching Algorithms.cpp
@@ -25,17 +25,36 @@ int binarySearch(vector<F> values, F number) {
     while (left <= right)
     {
         int middle = (left + right) / 2;
-        if (v[middle] < el)
+
+        if (values[middle] < number) {          // Na parte da direita
             left = middle + 1;
-        else if (el < v[middle]) right = middle – 1;
-        else
-            return middle; // found
+        } else if (number < values[middle]) {   // Na parte da esquerda
+            right = middle - 1;
+        } else {
+            return middle;
         }
     }
+    return -1;
 
+    // Time complexity: O(log2(n))
+    // Space complexity: O(1)
 }
 
 int main () {
+
+    // Test sequentialSearch() method
+    vector<int> numbers = {0, 3, 2, 4, 35, 52, 3, 6, 32, 52, 14, 634, 3, 54, 87, 23, 3, 8, 3, 0, 4, -4, 2, 7, 8, 2};
+    for (int i = 0 ; i < 10 ; i++ ) {
+        cout << i << "? " << sequentialSearch(numbers, i) << endl;
+    }
+
+    // Test binarySearch() method
+    vector<int> sortedNumbers = {0, 4, 6, 7, 12, 45, 67, 89, 133, 245, 246, 467, 6789, 23455};
+    vector<int> keys = {3, 7, 12, 245, 246, 247, 890, 23455};
+    for (auto key : keys) {
+        cout << key << "? " << binarySearch(sortedNumbers, key) << endl;
+    }
+
+
     return 0;
 }
-
