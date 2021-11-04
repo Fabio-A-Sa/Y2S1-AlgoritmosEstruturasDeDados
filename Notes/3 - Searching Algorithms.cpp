8e0d110fa5bdcb24ef3a959e9e7b4f654121e8c5
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-04 20:47:37 +0000

    More lines

--- Notes/3 - Searching Algorithms.cpp
+++ Notes/3 - Searching Algorithms.cpp
@@ -13,10 +13,25 @@ int sequentialSearch(vector<F> values, F number) {
         if (values[i] == number) return i;
     }
     return -1;
+
+    // Time complexity: O(n)
+    // Space complexity: O(1)
 }
 
 template <class F>
 int binarySearch(vector<F> values, F number) {
+
+    int left = 0, right = values.size() - 1;
+    while (left <= right)
+    {
+        int middle = (left + right) / 2;
+        if (v[middle] < el)
+            left = middle + 1;
+        else if (el < v[middle]) right = middle – 1;
+        else
+            return middle; // found
+        }
+    }
 
 }
 
