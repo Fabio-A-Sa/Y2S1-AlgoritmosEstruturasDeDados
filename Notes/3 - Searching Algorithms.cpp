88f04c61a770f75730eac1225a753782dea8022b
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-04 20:38:42 +0000

    Added linear search

--- Notes/3 - Searching Algorithms.cpp
+++ Notes/3 - Searching Algorithms.cpp
@@ -3,7 +3,19 @@
 // Theorical Lecture 5, 04/11/2021, Searching Algorithms
 
 #include <iostream>
+#include <vector>
 using namespace std;
+
+template <class F>
+int linearSearch(vector<F> values, F number) {
+
+    for (int i = 0 ; i < values.size() ; i++ ) {
+        if (values[i] == number) return i;
+    }
+    return -1;
+}
+
+
 
 int main () {
 
