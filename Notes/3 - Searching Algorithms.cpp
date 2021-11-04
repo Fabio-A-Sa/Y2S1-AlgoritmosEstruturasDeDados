9b481a2a22684b054cfaf5ed2c3b81ddaeb0abff
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-04 20:36:43 +0000

    Added file 3Q

---
+++ Notes/3 - Searching Algorithms.cpp
@@ -0,0 +1,15 @@
+// Created on November, 2021
+// @author: Fábio Araújo de Sá
+// Theorical Lecture 5, 04/11/2021, Searching Algorithms
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    for (int i = 0 ; i < 10 ; i++) {
+        cout << "Hello World" << endl;
+    }
+    return 0;
+}
+
