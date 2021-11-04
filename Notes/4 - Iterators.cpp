6eb01ad853b422d4268e2419cd16fe5ac4dbf045
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-04 22:14:21 +0000

    Added content

--- Notes/4 - Iterators.cpp
+++ Notes/4 - Iterators.cpp
@@ -1,3 +1,14 @@
 // Created on November, 2021
 // @author: Fábio Araújo de Sá
 // Theorical Lecture 5, 04/11/2021, Iterators
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main () {
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
