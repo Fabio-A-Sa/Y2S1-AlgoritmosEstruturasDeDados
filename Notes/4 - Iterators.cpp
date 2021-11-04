db64d83a6942ca290462a7c2763aecc85ed2157f
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-04 22:20:40 +0000

    DraftsQ

--- Notes/4 - Iterators.cpp
+++ Notes/4 - Iterators.cpp
@@ -7,8 +7,17 @@
 #include <algorithm>
 using namespace std;
 
+void sequentialSearch() {
+
+    vector<int> numbers = {23, 3, 123, 3, 1, 3, 5, 65, 32, 654, 246, 776, 3, 76, 457, 5, 975, 3224, 8, 2, 5, 42, 644, 75, 2, 44, 64};
+    vector<int> keys = {1, 2, 3, 4, 5, 6, 7, 8};
+    for (auto key : keys) {
+        cout << find(numbers.begin(), numbers.end()) << endl;
+    }
+}
+
 int main () {
 
-    cout << "Hello world!" << endl;
+    sequentialSearch();
     return 0;
 }
