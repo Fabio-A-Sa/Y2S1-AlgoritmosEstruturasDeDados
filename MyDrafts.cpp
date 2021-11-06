8099e09d94421a018ab06bcb7553a677a418ac08
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-06 11:56:41 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -399,3 +399,71 @@ public:
     unsigned getNumClientesAtuais() const;
 };
 
+ParqueEstacionamento::ParqueEstacionamento(unsigned lot, unsigned nMaxCli) {
+    this->lotacao = lot;
+    this->numMaximoClientes = nMaxCli;
+    this->vagas = lot;
+    this->clientes = {};
+}
+
+unsigned ParqueEstacionamento::getNumLugares() const {
+    return lotacao;
+}
+
+unsigned ParqueEstacionamento::getNumMaximoClientes() const {
+    return numMaximoClientes;
+}
+
+int ParqueEstacionamento::posicaoCliente(const string & nome) const {
+    for (unsigned i=0; i<clientes.size(); i++)
+        if (clientes[i].nome == nome) return i;
+    return -1;
+}
+
+bool ParqueEstacionamento::adicionaCliente(const string & nome) {
+    if (clientes.size() == numMaximoClientes) return false;
+    if (posicaoCliente(nome) != -1) return false;
+    InfoCartao info;
+    info.nome = nome;
+    info.presente = false;
+    clientes.push_back(info);
+    return true;
+}
+
+bool ParqueEstacionamento::entrar(const string & nome) {
+    if (!vagas) return false;
+    if (posicaoCliente(nome) == -1) return false;
+    if (clientes[posicaoCliente(nome)].presente) return false;
+    clientes[posicaoCliente(nome)].presente = true;
+    vagas --;
+    return true;
+}
+
+bool ParqueEstacionamento::retiraCliente(const string & nome) {
+
+    for (vector<InfoCartao>::iterator it = clientes.begin(); it != clientes.end(); it++)
+        if ( (*it).nome == nome ) {
+            if (!(*it).presente) {
+                clientes.erase(it);
+                return true;
+            }
+            else return false;
+        }
+    return false;
+}
+
+bool ParqueEstacionamento::sair(const string & nome)
+{
+    if (posicaoCliente(nome) == -1) return false;
+    if (!clientes[posicaoCliente(nome)].presente) return false;
+    clientes[posicaoCliente(nome)].presente = false;
+    vagas ++;
+    return true;
+}
+
+unsigned ParqueEstacionamento::getNumLugaresOcupados() const {
+    return lotacao - vagas;
+}
+
+unsigned ParqueEstacionamento::getNumClientesAtuais() const {
+    return clientes.size();
