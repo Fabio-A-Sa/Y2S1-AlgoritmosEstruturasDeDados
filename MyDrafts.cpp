bd04146a034a4283207a15762fbe52e1cf7eb3e6
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-06 11:56:22 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -371,3 +371,31 @@ int main () {
     testPerson();
     return 0;
 }
+
+class InfoCartao {
+
+public:
+    string nome;
+    bool presente;
+};
+
+class ParqueEstacionamento {
+
+    unsigned vagas;
+    unsigned  lotacao;
+    vector<InfoCartao> clientes;
+    unsigned numMaximoClientes;
+
+public:
+    ParqueEstacionamento(unsigned lot, unsigned nMaxCli);
+    bool adicionaCliente(const string & nome);
+    bool retiraCliente(const string & nome);
+    bool entrar(const string & nome);
+    bool sair(const string & nome);
+    int posicaoCliente(const string & nome) const;
+    unsigned getNumLugares() const;
+    unsigned getNumMaximoClientes() const;
+    unsigned getNumLugaresOcupados() const;
+    unsigned getNumClientesAtuais() const;
+};
+
