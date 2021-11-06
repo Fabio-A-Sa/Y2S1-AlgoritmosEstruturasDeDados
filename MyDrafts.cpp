9c9237f0fe4c402c84f73c70d34b6f91d4fd3ef5
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-06 11:52:39 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -1,9 +1,42 @@
+// Created on October, 2021
+
 #include <iostream>
+#include <cmath>
+#include <vector>
+#include <ctime>
+#include <algorithm>
 using namespace std;
 
-int main ()
-{
-    cout << "Hello World!" << endl;
+template <class Number>
+Number squareRoot (Number n) { return sqrt(n); }
+
+// invariant: em cada iteração, sum = 1+2+…+(k-1)
+// variant: n + 1 - k, pois o valor máximo de K será n + 1 e K é incrementado a cada iteração
+
+long sum (int n) {
+    long sum = 0;
+    int k = 1;
+    while (k <= n) {
+        sum += k;
+        k++;
+    }
+    return sum;
+}
+
+int main () {
+
+    double x = 25.651;
+    double result = squareRoot(25.651);
+    cout << "\n\nPreconditions and postconditions\n" << endl;
+    cout << "X = " << x << endl << "Result = squareRoot(X) = " << result << endl;
+    cout << "Preconditions: x >= 0\nPostconditions: result >= 0 && pow(result, 2) == x\n" << endl;
+
+    cout << "Parcial correctness:\n\t- Com as pré-condições correctas, caso o algoritmo termine então está correcto;" << endl;
+    cout << "\t- Detetar a invariante do ciclo e provar a veracidade para o início, meio e fim;\n" << endl;
+    cout << "Total correctness:\n\t- Com as pré-condições correctas provar que o algoritmo termina;" << endl;
+    cout << "\t- Detetar a invariante do ciclo e provar a veracidade para o início, meio e fim;" << endl;
+    cout << "\t- Detetar a variante do ciclo e provar que é uma expressão inteira, não-negativa e decrescente;\n\n" << endl;
+
     return 0;
 }
 
