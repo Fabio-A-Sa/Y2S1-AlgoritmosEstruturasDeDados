1f3efe8749b211fc7911642611bfffc8c2675203
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-08 18:48:08 +0000

    Begin

--- Notes/5 - Sorting Algorithms.cpp
+++ Notes/5 - Sorting Algorithms.cpp
@@ -6,16 +6,36 @@
 #include <vector>
 #include <string>
 #include <random>
+#include <ctime>
 
 using namespace std;
 
-void showVector () {
+void showVector (const vector<int> &vector) {
+    for (auto number : vector) {
+        cout << number << " ";
+    }
+    cout << endl << endl;
+}
+
+void insertionSort (vector<int> numbers) {
+
     
+    cout << "Insertion Sort: " << endl; showVector(numbers);
 }
 
 int main () {
 
+    srand(time(NULL));
+    int min = -5, max = 100, size = 20;
     vector<int> numbers = {};
+
+    // Fill vector with random numbers
+    for (int i = 0 ; i < size ; i++) {
+        numbers.push_back(min + (rand() % (max - min)));
+    }
+    cout << "\nInitial vector" << endl; showVector(numbers);
+
+    insertionSort(numbers);
 
     return 0;
 }
