2b58f098c5d8efea02579276fed87dcf791a0e70
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-09 09:10:12 +0000

    Draft update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -1098,3 +1098,147 @@ EXPECT_EQ(13, fibonacci_2(7));
 EXPECT_EQ(144, fibonacci_2(12));
 EXPECT_EQ(2584, fibonacci_2(18));
 }
+
+void showVector (const vector<int> &vector) {
+    for (auto number : vector) {
+        cout << number << " ";
+    }
+    cout << endl << endl;
+}
+
+void insertionSort (vector<int> numbers) {
+
+    for (int i = 1 ; i < numbers.size() ; i++ ) {
+        int temp = numbers[i], j;
+        for (j = i ; j > 0 && temp < numbers[j-1] ; j-- ) {
+            numbers[j] = numbers[j-1];
+        }
+        numbers[j] = temp;
+    }
+    cout << "Insertion Sort: " << endl;
+    showVector(numbers);
+    // Time complexity: O(n^2)
+    // Space complexity: O(1)
+}
+
+void selectionSort (vector<int> numbers) {
+
+    for (int i = 0 ; i < numbers.size() ; i++ ) {
+        int min = i;
+        for (int j = i+1 ; j < numbers.size() ; j++ ) {
+            if (numbers[j] < numbers[min]) min = j;
+        }
+        int temp = numbers[i];
+        numbers[i] = numbers[min];
+        numbers[min] = temp;
+    }
+    cout << "Selection Sort: " << endl;
+    showVector(numbers);
+    // Time complexity: O(n^2)
+    // Space complexity: O(1)
+}
+
+void bubbleSort (vector<int> numbers) {
+
+    bool swap = true;
+    while (swap) {
+        swap = false;
+        for (int i = 0 ; i < numbers.size()-1 ; i++ ) {
+            for (int j = i + 1 ; j < numbers.size() ; j++ ) {
+                if (numbers[i] > numbers[j]) {
+                    int temp = numbers [i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                    swap = true;
+                }
+            }
+        }
+    }
+    cout << "Bubble Sort: " << endl;
+    showVector(numbers);
+    // Time complexity: O(n^2)
+    // Space complexity: O(1)
+}
+
+void shellSort (vector<int> numbers) {
+
+    int j;
+    for (int gap = numbers.size() / 2 ; gap > 0 ; gap /= 2) {
+        for (int i = gap ; i < numbers.size() ; i++ ) {
+            int temp = numbers[i];
+            for (j = i ; j >= gap && temp < numbers[j-gap] ; j -= gap) {
+                numbers[j] = numbers[j-gap];
+            }
+            numbers[j] = temp;
+        }
+    }
+    cout << "Shell Sort: " << endl;
+    showVector(numbers);
+    // Time complexity: O(n^2)
+    // Space complexity: O(1)
+}
+
+void merge(vector <int> &v, vector<int> &tmpArr, int leftPos, int rightPos, int rightEnd) {
+
+    int leftEnd = rightPos--, tmpPos = leftPos;
+    int numElements = rightEnd - leftPos + 1;
+    while ( leftPos <= leftEnd && rightPos <= rightEnd )
+        if ( v[leftPos] <= v[rightPos] )
+            tmpArr[tmpPos++] = v[leftPos++];
+        else
+            tmpArr[tmpPos++] = v[rightPos++];
+    while ( leftPos <= leftEnd )
+        tmpArr[tmpPos++] = v[leftPos++];
+    while ( rightPos <= rightEnd )
+        tmpArr[tmpPos++] = v[rightPos++];
+    for ( int i = 0; i < numElements; i++, rightEnd-- )
+        v[rightEnd] = tmpArr[rightEnd];
+}
+
+void mergeSort(vector <int> &v, vector<int> &tmpArr, int left, int right) {
+    if (left < right) {
+        int center = (left + right) / 2;
+        mergeSort(v, tmpArr, left, center);
+        mergeSort(v, tmpArr, center + 1, right);
+        merge(v, tmpArr, left, center +1, right);
+    }
+}
+
+void mergeSortMain(vector <int> v) {
+    vector<int> tmpArr(v.size());
+    mergeSort(v, tmpArr, 0, v.size()-1);
+    cout << "Merge Sort: " << endl;
+    showVector(v);
+    // Time complexity: O(n*log(n))
+    // Space complexity: O(n)
+}
+
+void quickSort (vector<int> numbers) {
+
+    // TODO
+    cout << "Quick Sort: " << endl;
+    showVector(numbers);
+}
+
+int main () {
+
+    srand(time(NULL));
+    int min = -5, max = 100, size = 20;
+    vector<int> numbers = {};
+
+    // Fill vector with random numbers
+    for (int i = 0 ; i < size ; i++) {
+        numbers.push_back(min + (rand() % (max - min)));
+    }
+    cout << "\nInitial vector" << endl; showVector(numbers);
+
+    // Tests
+    insertionSort(numbers);
+    selectionSort(numbers);
+    bubbleSort(numbers);
+    shellSort(numbers);
+    mergeSortMain(numbers);
+    quickSort(numbers);
+
+    return 0;
+}
