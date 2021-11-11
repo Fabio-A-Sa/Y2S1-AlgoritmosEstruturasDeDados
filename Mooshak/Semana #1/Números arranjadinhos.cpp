d6b1582f178155ecf865b3a6bb91c7b6a325796a
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-11 12:59:53 +0000

    Exercises 2

--- Mooshak/Semana #1/Números arranjadinhos.cpp
+++ Mooshak/Semana #1/Números arranjadinhos.cpp
@@ -1,4 +1,31 @@
-//
-// Created by Fábio Sá on 11/11/2021.
-//
+// Created on 11 November, 2021
+// author: Fábio Araújo de Sá, up202007658
 
+#include <iostream>
+using namespace std;
+
+bool match (long int attemp, int sum) {
+
+    int currentSum = 0, digit;
+    while (attemp) {
+        digit = attemp % 10;
+        attemp = attemp / 10;
+        currentSum += digit;
+    }
+    return currentSum == sum;
+}
+
+int main() {
+
+    long int number;
+    int sum, lines;
+
+    cin >> lines;
+    for (int i = 0 ; i < lines ; i++) {
+        cin >> number >> sum;
+        while (!match(number + 1, sum)) number++;
+        cout << number + 1 << endl;
+    }
+
+    return 0;
+}
