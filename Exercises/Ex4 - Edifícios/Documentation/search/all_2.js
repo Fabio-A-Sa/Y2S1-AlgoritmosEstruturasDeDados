ff76f4acad9e17b619fad88791c7d091c684c244
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-12 23:46:34 +0000

    Doxygen documentation

---
+++ Exercises/Ex4 - Edifícios/Documentation/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['smallestmissingvalue_0',['smallestMissingValue',['../class_fun_search_problem.html#a89555d6d9110d813b86b8099255d6551',1,'FunSearchProblem']]],
+  ['squarer_1',['squareR',['../class_fun_search_problem.html#a67eee7c9f89c404f54c439e86a2bfa59',1,'FunSearchProblem']]]
+];
---
+++ Exercises/Ex4 - Edifícios/Documentation/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['minpages_0',['minPages',['../class_fun_search_problem.html#a2ad335cdf90c65b72ec76ba46acb660b',1,'FunSearchProblem']]]
+];
