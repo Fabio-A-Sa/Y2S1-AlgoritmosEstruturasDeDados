74f02aa4092cb78c560f2225a618298092d99e28
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-13 15:08:24 +0000

    More changes

--- Mooshak/Semana #1/Matrículas.cpp
+++ Mooshak/Semana #1/Matrículas.cpp
@@ -7,6 +7,7 @@
 #include <cmath>
 #include <typeinfo>
 #include <algorithm>
+
 using namespace std;
 
 const vector<char> letters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
@@ -15,22 +16,33 @@ const vector<char> letters = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
 
 const vector<int> numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-const int diffBetweenTypes = pow(numbers.size(), 4) * pow(letters.size(), 2);
+const int diffBetweenTypes = pow(numbers.size(), 4) * pow(letters.size(), 2) ;
 
-int findType (string &matricula) { // Find type (1, 2, 3, 4) and modify matricula to type 1 to compare later
+/**
+ * Type 1: XX-NN-NN
+ * Type 2: NN-NN-XX
+ * Type 3: NN-XX-NN
+ * Type 4: XX-NN-XX
+ * myType: XX-NNNN or XXXX-NN quando aplicável
+ */
+int findType (string & m) { // Find type (1, 2, 3, 4) and normalize to myType to compare later
+
+    int type;
+    string p1 = m.substr(0, 2), p2 = m.substr(3, 2), p3 = m.substr(6, 2);
+    cout << p2 <<
     return 1;
 }
 
-int diffInt(int c1, int c2) {
-    return abs(c2-c1);
+int diffChar(char c1, char c2) {
+    int indexC1, indexC2;
+    for (int i = 0 ; i < letters.size() ; i++) {
+        if (letters[i] =)
+    }
+    return abs(indexC1 - indexC2);
 }
 
-int diffChar(char c1, char c2) {
-    int  = 0;
-    for (int i = 0 ; i < letters.size() ; i++) {
-        if ()
-    }
-    return abs();
+long int getDiff(string m1, string m2) {
+    return 0;
 }
 
 int main () {
@@ -41,7 +53,7 @@ int main () {
     cin >> lines;
     for (int i = 0 ; i < lines ; i++) {
         cin >> matricula1 >> matricula2;
-        cout << matricula2 << endl;
+        cout << getDiff(matricula1, matricula2) << endl;
     }
 
     return 0;
