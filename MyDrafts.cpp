571fb36118a27948324ee0af81333572bce704c0
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-14 00:08:59 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -1849,3 +1849,35 @@ float Piece::getDiameter() const {
     return diameter;
 }
 
+class Piece {
+
+    string id;
+    float diameter;
+
+public:
+    Piece(string i, float d);
+    string getId() const;
+    float getDiameter() const;
+};
+
+Product::Product(string i, float p, float w): id(i), price(p), weight(w) {}
+
+string Product::getId() const {
+    return id;
+}
+
+float Product::getPrice() const {
+    return price;
+}
+
+float Product::getWeight() const {
+    return weight;
+}
+
+bool Product::operator < (const Product &p) const {
+    if (this->getPrice() == p.getPrice()) {
+        return this->getWeight() < p.getWeight();
+    }
+    return this->getPrice() < p.getPrice();
+}
+
