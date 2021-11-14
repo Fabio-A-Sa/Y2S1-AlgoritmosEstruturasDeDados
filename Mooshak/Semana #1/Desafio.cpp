745408211c01cf9b74e70a83a1a39898c9a4ea1b
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-14 14:59:21 +0000

    Desafio

--- Mooshak/Semana #1/Desafio.cpp
+++ Mooshak/Semana #1/Desafio.cpp
@@ -1,4 +1,4 @@
-// Created on 11 November, 2021
+// Created on 14 November, 2021
 // author: Fábio Araújo de Sá, up202007658
 
 #include <iostream>
@@ -6,9 +6,17 @@
 #include <cmath>
 using namespace std;
 
-const int NUM_CASAS = 18;
+/**
+ * Considerando as restrições do Problema, o maior valor inicial será 10^16 e o intervalo máximo 10^18,
+ * pelo que o valor a retornar terá, no máximo, 18 algarismos.
+ */
+const int NUM_ALGA = 18;
 
-int sume(long long int attemp) {
+/**
+ * Uma função simples que retorna a soma de todos os dígitos do @param attemp
+ * Como a soma dos dígitos está compreendida no intervalo [0..100], basta retornar um inteiro (e não long ou long long)
+ */
+int sumN(long long int attemp) {
 
     int currentSum = 0, digit;
     while (attemp) {
@@ -19,19 +27,29 @@ int sume(long long int attemp) {
     return currentSum;
 }
 
-void showVector(vector<int> digits) {
-    for (auto digit : digits) cout << digit << " ";
-    cout << endl;
-}
+/**
+ * Descobri a solução para o problema ontem quando estudava o CountingSort. Ao colocar os números dentro de
+ * "caixas" lembrei que poderia usar esse esquema para colocar cada algarismo do long long int apresentado como input
+ * num vector de inteiros. Assim é fácil manipular cada dígito separadamente até atingir a soma pedida sem chamar sempre
+ * a função sumN().
+ */
+long long int solve (long long int num, int sum) {
 
-void solve (long long int num, int sum) {
-
-    vector<int> digits(NUM_CASAS);
-    int casa = digits.size() - 1, digit;
+    vector<int> digits(NUM_ALGA);               // O vector que irá guardar os dígitos.
+    int casa = digits.size() - 1, digit;        // "casa" será o index que percorrerá os dígitos do fim para o início
     int currentSum = 0;
 
-    while (sume(num) > sum) num++;
-    while (num) {           // Cuidado: tem de ser um número maior
+    /**
+     * Um problema recorrente é quando a soma atual do valor dado como input já é maior do que a soma pedida.
+     * Assim optei por somar iterativamente até que a situação ficasse favorável ao problema.
+     */
+    while (sumN(num) >= sum) num++;
+
+    /**
+     * O vector digits fica com cada um dos dígitos iniciais do valor.
+     * CurrentSum ficará também com a soma dos dígitos atual.
+     */
+    while (num) {
         digit = num % 10;
         digits[casa] = digit;
         casa--;
@@ -39,13 +57,12 @@ void solve (long long int num, int sum) {
         currentSum += digit;
     }
 
+    casa = digits.size() - 1;           // Volta a apontar para o último dígito
+    int sobra = sum - currentSum;       // Sobra é o número que terei de incrementar ao vector até prefazer a soma pretendida
 
-    casa = digits.size() - 1;
-    int sobra = sum - currentSum;
+    while (sobra) {                     // Enquanto houver número a incrementar a dígitos
 
-    while (sobra) {
-
-        int possivel = 9 - digits[casa]; // possível --> o que falta para preencher a casa
+        int possivel = 9 - digits[casa]; // possível --> o que falta para preencher a casa até um máximo de 9
         if (sobra >= possivel) {
             digits[casa] += possivel;
             sobra -= possivel;
@@ -56,7 +73,7 @@ void solve (long long int num, int sum) {
         casa--;
     }
 
-    long long int result = 0;
+    long long int result = 0;           // Reconstrução do número, agora com a soma correcta
     int index = digits.size() - 1;
     int exponent = 0;
     while (index) {
@@ -64,31 +81,31 @@ void solve (long long int num, int sum) {
         index--;
         exponent++;
     }
-    cout << result << endl;
+    return result;
 
 }
 
 int main() {
 
-    /**
+    /** Solve() tests:
+   long long int number;
+   int sum, lines;
 
-    long int number;
-    int sum, lines;
+   cin >> lines;
+   for (int i = 0 ; i < lines ; i++) {
+       cin >> number >> sum;
+       cout << solve(number, sum) << endl;
+   }
+      */
 
-    cin >> lines;
-    for (int i = 0 ; i < lines ; i++) {
-        cin >> number >> sum;
-        cout << solve(number, sum) << endl;
-    }
-          *
-     */
-    solve(100000599988, 50); cout << endl;
-    solve(100000599989, 50); cout << endl;
-    solve(100000599998, 50); cout << endl;
-    solve(987654321098, 100); cout << endl;
-    solve(12121678909876, 42); cout << endl;
-    solve(12121128909876, 42); cout << endl;
-    solve(1, 92); cout << endl;
+   cout << solve(100000599988, 50) << endl;
+   cout << solve(100000599989, 50) << endl;
+   cout << solve(100000599998, 50) << endl;
+   cout << solve(987654321098, 100) << endl;
+   cout << solve(12121678909876, 42) << endl;
+   cout << solve(12121128909876, 42) << endl;
+   cout << solve(1, 92) << endl;
+
 
     return 0;
 }
