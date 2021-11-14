badebec30c696522c87ec3c4c4b148944a114710
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-14 00:09:07 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -1860,24 +1860,3 @@ public:
     float getDiameter() const;
 };
 
-Product::Product(string i, float p, float w): id(i), price(p), weight(w) {}
-
-string Product::getId() const {
-    return id;
-}
-
-float Product::getPrice() const {
-    return price;
-}
-
-float Product::getWeight() const {
-    return weight;
-}
-
-bool Product::operator < (const Product &p) const {
-    if (this->getPrice() == p.getPrice()) {
-        return this->getWeight() < p.getWeight();
-    }
-    return this->getPrice() < p.getPrice();
-}
-
