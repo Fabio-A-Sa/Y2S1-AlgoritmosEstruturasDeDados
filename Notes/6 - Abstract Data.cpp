6b031a18a4df8ce41661ebb45317fa12690f9e66
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-16 21:25:38 +0000

    Using lists

--- Notes/6 - Abstract Data.cpp
+++ Notes/6 - Abstract Data.cpp
@@ -1,11 +1,26 @@
 // Created on November, 2021
 // @author: Fábio Araújo de Sá
 
+#include <iostream>
+#include <algorithm>
+#include <list>
+
+using namespace std;
+
 void AbstractData() {
 
+    cout << "Arrays:" << endl;
+    char[] name = {'F', 'E', 'U', };
+
     cout << "Lists:" << endl;
+    list<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    for (auto number : numbers) cout << number << " ";
+    cout << endl;
+
 }
 
 int main () {
+
+    AbstractData();
     return 0;
 }
