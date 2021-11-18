5e283f3f7b02e918adb302d7aa5b16e884281025
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-18 16:50:00 +0000

    Ex1, alinea a

--- Exercises/Ex6 - Kids/game.cpp
+++ Exercises/Ex6 - Kids/game.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 
 unsigned int Game::numberOfWords(string phrase) {
+
   if ( phrase.length() == 0 ) return 0;
 
   unsigned n=1;
@@ -26,23 +27,25 @@ Game::Game(list<Kid>& l2) {
 	this->kids = l2;
 }
 
-//-----------------------------------------------------------------
-
-// TODO
 void Game::addKid(const Kid k1) {
+    kids.push_back(k1);
 }
 
-// TODO
 list<Kid> Game::getKids() const {
-    return (list<Kid>());
+    return kids;
 }
 
-// TODO
 void Game::setKids(const list<Kid>& l1) {
+    this->kids = l1;
 }
 
-// TODO
 Kid Game::loseGame(string phrase) {
+
+    int words = numberOfWords(phrase);
+    while (kids.size() > 1) {
+        
+    }
+
     return (Kid("",0,'x'));
 }
 
