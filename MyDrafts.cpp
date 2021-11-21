305229606b597832d479d65e11a3c2b95be0e289
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-21 23:26:42 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -2202,3 +2202,87 @@ int main () {
 
     return 0;
 }
+
+void showResult (vector<int> result, int question) {
+
+    if (!result.empty()) {
+
+        if (result.size() == 1) {
+            cout << result[result.size()-1] << endl;
+        } else {
+            int minRange = abs(question - result[result.size()-1]);
+            for (int i = 0 ; i < result.size() ; i++ ) {
+                if (result[i] == result[result.size()-1]) {
+                    cout << result[i];
+                } else {
+                    if (abs(result[i]-question) == minRange) {
+                        cout << result[i] << " ";
+                    }
+                }
+
+            }
+            cout << endl;
+        }
+
+    } else cout << endl;
+}
+
+vector<int> solve (vector<int> numbers, int question) {
+
+    vector<int> result = {};
+    int a, b, minRange = 2147483647; // Mooshak não reconhece o INT_MAX?!
+    for (int i = 0 ; i < numbers.size() ; i++) {
+        for (int j = i + 1 ; j < numbers.size() ; j++) {
+            if (abs(numbers[i]+numbers[j]-question) <= minRange) {
+                a = numbers[i];
+                b = numbers[j];
+                minRange = abs(a+b-question);
+                result.push_back(a+b);
+            }
+        }
+    }
+
+    // Remove duplicates using iterators
+    for (vector<int>::iterator i = result.begin() ; i != result.end() ; i++) {
+        for (vector<int>::iterator j = i + 1 ; j != result.end() ; j++) {
+            if (*i == *j) {
+                result.erase(j);
+                j--;
+            }
+        }
+    }
+
+    return result;
+}
+
+int main () {
+
+    int nNumbers, currentNumber, nQuestions, currentQuestion;
+    vector<int> numbers = {}, questions = {};
+
+    cin >> nNumbers;
+    for (int i = 0 ; i < nNumbers ; i++) {
+        cin >> currentNumber;
+        numbers.push_back(currentNumber);
+    }
+    sort(numbers.begin(), numbers.end());
+
+    cin >> nQuestions;
+    for (int i = 0 ; i < nQuestions ; i++) {
+        cin >> currentQuestion;
+        questions.push_back(currentQuestion);
+    }
+
+    /**
+    numbers = {5, 6, 8, 9};
+    questions = {1, 2, 14, 16, 18, 1000};
+    sort(numbers.begin(), numbers.end());
+    */
+
+    for (auto question : questions) {
+        vector<int> result = solve (numbers, question);
+        showResult(result, question);
+    }
+
+    return 0;
+}
