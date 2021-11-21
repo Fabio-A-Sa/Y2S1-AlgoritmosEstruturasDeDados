9600f7ed5f5b895798f152df135a9da68de882d1
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-21 20:15:28 +0000

    Before submition

--- Mooshak/Semana #2/Somas mais próximas.cpp
+++ Mooshak/Semana #2/Somas mais próximas.cpp
@@ -6,34 +6,53 @@
 #include <algorithm>
 using namespace std;
 
-void showResult (vector<int> result) {
-    for (auto number : result) cout << number << " ";
-    cout << endl;
+void showResult (vector<int> result, int question) {
+
+    if (!result.empty()) {
+
+        if (result.size() == 1) {
+            cout << result[result.size()-1] << endl;
+        } else {
+            int minRange = abs(question - result[result.size()-1]);
+            for (int i = 0 ; i < result.size() ; i++ ) {
+                if (result[i] == result[result.size()-1]) {
+                    cout << result[i];
+                } else {
+                    if (abs(result[i]-question) == minRange) {
+                        cout << result[i] << " ";
+                    }
+                }
+
+            }
+            cout << endl;
+        }
+
+    } else cout << endl;
 }
 
 vector<int> solve (vector<int> numbers, int question) {
 
     vector<int> result = {};
-    int a, b, minRange = INT_MAX;
+    int a, b, minRange = 2147483647;
     for (int i = 0 ; i < numbers.size() ; i++) {
         for (int j = i + 1 ; j < numbers.size() ; j++) {
-            if (abs(numbers[i]+numbers[j]-question) < minRange) {
-                minRange = abs(a+b-question);
+            if (abs(numbers[i]+numbers[j]-question) <= minRange) {
                 a = numbers[i];
                 b = numbers[j];
+                minRange = abs(a+b-question);
+                result.push_back(a+b); // sempre que troca
             }
         }
     }
-    result.push_back(a+b);
     return result;
 }
 
 int main () {
 
-    /**
     int nNumbers, currentNumber, nQuestions, currentQuestion;
     vector<int> numbers = {}, questions = {};
 
+    /**
     cin >> nNumbers;
     for (int i = 0 ; i < nNumbers ; i++) {
         cin >> currentNumber;
@@ -48,13 +67,14 @@ int main () {
     }
     */
 
-    vector<int> numbers = {12, 3, 17, 5, 34, 33};
-    vector<int> questions = {1, 51, 41, 21};
+    numbers = {5 6 8 9};
+    questions = {1 2 14 16 18 1000};
     sort(numbers.begin(), numbers.end());
+
 
     for (auto question : questions) {
         vector<int> result = solve (numbers, question);
-        showResult(result);
+        showResult(result, question);
     }
 
     return 0;
