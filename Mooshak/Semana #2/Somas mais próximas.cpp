4eceee83721aa0b7d91b52276a86d12d66beb6b0
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-21 14:10:47 +0000

    DRfst

--- Mooshak/Semana #2/Somas mais próximas.cpp
+++ Mooshak/Semana #2/Somas mais próximas.cpp
@@ -3,8 +3,41 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
+vector<int> solve (vector<int> numbers, int question) {
+
+    vector<int> result = {};
+    int a, int b, int minRange = INT_MAX;
+    for (int i = 0 ; i < numbers.size() ; i++) {
+        for (int j = i + 1 ; j < numbers.size() ; j++) {
+            if (abs() < minRange) {
+                minRange =
+            }
+        }
+    }
+
+    return result;
+}
+
 int main () {
+
+    int nNumbers, currentNumber, nQuestions, currentQuestion;
+    vector<int> numbers = {}, questions = {};
+
+    cin >> nNumbers;
+    for (int i = 0 ; i < nNumbers ; i++) {
+        cin >> currentNumber;
+        numbers.push_back(currentNumber);
+    }
+    sort(numbers.begin(), numbers.end());
+
+    cin >> nQuestions;
+    for (int i = 0 ; i < nQuestions ; i++) {
+        cin >> currentQuestion;
+        questions.push_back(currentQuestion);
+    }
+
     return 0;
 }
