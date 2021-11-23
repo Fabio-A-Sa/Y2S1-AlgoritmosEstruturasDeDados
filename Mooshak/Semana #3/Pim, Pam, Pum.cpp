25f42139062ef3c8ae6d728c2785003f86eb09c4
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-23 21:26:48 +0000

    Get number of words

--- Mooshak/Semana #3/Pim, Pam, Pum.cpp
+++ Mooshak/Semana #3/Pim, Pam, Pum.cpp
@@ -1,7 +1,19 @@
 // Created on 23 November, 2021
 // author: Fábio Araújo de Sá, up202007658
 
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int getWords (const string &phrase) {
+    int result = 0;
+    for (auto letter : phrase) if (letter == ' ') result++;
+    return result++;
+}
+
 int main () {
 
+    
     return 0;
 }
