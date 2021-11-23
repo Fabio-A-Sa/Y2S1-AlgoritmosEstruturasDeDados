e05f9e73a0f809f45db5491f571f931e5bc783f5
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-23 21:43:29 +0000

    my solution

--- Mooshak/Semana #3/Pim, Pam, Pum.cpp
+++ Mooshak/Semana #3/Pim, Pam, Pum.cpp
@@ -2,7 +2,7 @@
 // author: Fábio Araújo de Sá, up202007658
 
 #include <iostream>
-#include <vector>
+#include <list>
 #include <string>
 using namespace std;
 
@@ -12,8 +12,48 @@ int getWords (const string &phrase) {
     return result++;
 }
 
+string getLoser (const string& phase, const list<string> &kids) {
+
+    int words = getWords(phrase);
+    int index = 0;
+
+    while (kids.size() > 1) {
+        index = (words-1+index) % kids.size();
+        list<Kid>::iterator it = kids.begin();
+        for (int i = 0 ; i < index ; i++) { it++; }
+        kids.remove(*it);
+    }
+
+    return kids.front();
+}
+
+void showResults (const string& phrase, const list<string> &kids) {
+
+    string loser = getLoser(phrase, kids);
+    if (loser == "Carlos") {
+       cout << "O Carlos nao se livrou" << endl;
+    } else {
+        cout << "O Carlos livrou-se (coitado do " << loser << "!)" << endl;
+    }
+}
+
 int main () {
 
-    
+    list<string> names = {};
+    int loops, nNames;
+    string phrase, currentName;
+    cin >> loops;
+
+    for (int i = 0 ; i < loops ; i++) {
+        getline(cin, phrase);
+        cin >> nNames;
+        for (int j = 0 ; j < nNames ; j++) {
+            cin >> currentName;
+            names.push_back(currentName);
+        }
+        showResults(phrase, names);
+        names.clear();
+    }
+
     return 0;
 }
