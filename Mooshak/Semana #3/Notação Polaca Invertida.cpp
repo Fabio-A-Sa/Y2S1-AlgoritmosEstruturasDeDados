964f665fadf956113e538696ec505c2d67be29fd
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-23 23:17:34 +0000

    Some drafs

--- Mooshak/Semana #3/Notação Polaca Invertida.cpp
+++ Mooshak/Semana #3/Notação Polaca Invertida.cpp
@@ -4,21 +4,61 @@
 #include <iostream>
 #include <string>
 #include <stack>
+#include <vector>
 using namespace std;
 
-void solve (const string &expression) {
-    cout << "Test" << endl;
+int calculator(int a, int b, char operation) {
+
+    switch (operation) {
+        case '+': return a+b;
+        case '-': return a-b;
+        case '/': return a/b;
+        case '*': return a*b;
+        default: return 0;
+    }
+}
+
+void solve (const vector<char> &expression) {
+
+    stack<int> numbers = {};
+
+    for (char c : expression) {
+        if (c != ' ') {
+
+            if (isdigit(c)) {
+                numbers.push(c-48); // char to int using ASCII code :)
+            } else {
+                if (numbers.size() > 1) {
+                    int a = numbers.top();
+                    numbers.pop();
+                    int b = numbers.top();
+                    numbers.pop();
+                    int result = calculator(a, b, c);
+                    numbers.push(result);
+                } else {
+                    cout << "Expressao Incorrecta" << endl;
+                    return;
+                }
+            }
+        }
+    }
+    cout << numbers.top() << endl;
+    numbers.pop();
 }
 
 int main () {
 
+    vector<char> expression = {};
     int loops;
     string currentExpression;
     cin >> loops;
 
     for (int i = 0 ; i < loops ; i++) {
-        cin >> currentExpression;
+        getline(cin, currentExpression);
+        cin.clear();
+        cin.ignore(1000, '\n');
         solve(currentExpression);
     }
+
     return 0;
 }
