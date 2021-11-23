a84712a5440330d5e84183ecf1678a903cc2bb4f
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-23 21:13:17 +0000

    Exercise 2

--- Exercises/Ex6 - Kids/stackExt.h
+++ Exercises/Ex6 - Kids/stackExt.h
@@ -7,42 +7,47 @@ using namespace std;
 
 template <class T>
 class StackExt {
+
 	stack<T> values;
 	stack<T> minimums;
-public:
-	StackExt() {};
-	bool empty() const; 
-	T& top();
-	void pop();
-	void push(const T& val);
-	T& findMin();
+
+    public:
+        StackExt() {};
+        bool empty() const;
+        T& top();
+        void pop();
+        void push(const T& val);
+        T& findMin();
 };
 
-//TODO
 template <class T> 
 bool StackExt<T>::empty() const {
-	return true;
+	return this->values.empty();
 }
 
-//TODO
 template <class T> 
 T& StackExt<T>::top() {
-    return *new T();
+    T& result = this->values.top();
+    return result;
 }
 
-//TODO
 template <class T> 
 void StackExt<T>::pop() {
+    if (this->minimums.top() == this->values.top()) {
+        this->minimums.pop();
+    }
+    this->values.pop();
 }
 
-//TODO
 template <class T> 
 void StackExt<T>::push(const T& val) {
+    if (this->minimums.empty()) this->minimums.push(val);
+    this->values.push(val);
+    if (val < this->minimums.top()) this->minimums.push(val);
 }
 
-//TODO
 template <class T> 
 T& StackExt<T>::findMin() {
-    return *new T();
+    T& result = this->minimums.top();
+    return result;
 }
-
