933e0d4a2683f5959b8e68881cd2836c2be6a054
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-23 21:16:45 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -2480,3 +2480,22 @@ string Kid::write() const {
     return oss.str();
 }
 
+class Kid {
+
+    string name;
+    int age;
+    char sex;
+
+public:
+    Kid();
+    Kid(string nm, unsigned a, char s);
+    Kid(const Kid& k1);
+    string getName() const;
+    unsigned getAge() const;
+    char getSex() const;
+    bool operator == (const Kid& k2) const;
+    string write() const;
+    friend ostream & operator<<(ostream & o, const Kid & d);
+};
+
+
