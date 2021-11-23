4a995cf83797072f5cc08c932886222cae9f5029
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-23 20:07:44 +0000

    DRfats

--- Notes/6 - Abstract Data.cpp
+++ Notes/6 - Abstract Data.cpp
@@ -9,9 +9,18 @@
 
 using namespace std;
 
+class SegmentationFaultException : public Exception {
+    public:
+        SegmentationFaultException();
+        void showError();
+};
+
+SegmentationFaultException::SegmentationFaultException() {};
+void SegmentationFaultException::showError() { cout << "Error. pop() not allowed in empty stack!" << endl; }
+
 void Arrays() {
 
-    cout << "Arrays:" << endl;
+    cout << endl << "Arrays:" << endl;
     char name[] = {'F', 'E', 'U', 'P'};
     for (auto letter : name) cout << letter;
     cout << endl;
@@ -19,7 +28,7 @@ void Arrays() {
 
 void Lists() {
 
-    cout << "Lists:" << endl;
+    cout << endl << "Lists:" << endl;
     list<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     for (auto number : numbers) cout << number << " ";
     cout << endl;
@@ -27,26 +36,32 @@ void Lists() {
 
 void Stack() {
 
-    cout << "Stack:" << endl;
+    cout << endl << "Stack:" << endl;
     stack<int> numbers = {};
+
+    try {
+        numbers.pop();
+    } catch (SegmentationFaultException e) {
+        e.showError();
+    }
+
     cout << "Input:" << endl;
     for (int i = 0 ; i < 10 ; i++) {
         cout << i << " ";
         numbers.push(i);
     }
-    
-    cout << end << "Output:" << endl;
+
+    cout << endl << "Output:" << endl;
     while (!numbers.empty()) {
         cout << numbers.top() << " ";   // Show header number
         numbers.pop();                  // Erase header number
     }
     cout << endl;
-
 }
 
 void Queues() {
 
-    cout << "Queue:" << endl;
+    cout << endl << "Queue:" << endl;
 }
 
 int main () {
