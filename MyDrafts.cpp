97926727565f15b6958b3673f521e5beff97f3fd
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-23 21:16:18 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -2429,3 +2429,22 @@ list<Kid> Game::shuffle() const {
     return result;
 }
 
+class Game
+{
+    list<Kid> kids;
+
+public:
+    Game();
+    Game(list<Kid>& l2);
+    static unsigned numberOfWords(string phrase);
+    void addKid(const Kid k1);
+    list<Kid> getKids() const;
+    void setKids(const list<Kid>& l1);
+    Kid loseGame(string phrase);
+    list<Kid> removeOlder(unsigned id);
+    queue<Kid> rearrange();
+    bool operator==(Game& g2);
+    list<Kid> shuffle() const;
+    void splitKids(queue<Kid> &a, queue<Kid> &b) const;
+};
+
