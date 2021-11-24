90e251fc1c96b844795371b2eb35a40752c96fb2
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-24 18:38:32 +0000

    Drafts

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -2544,3 +2544,57 @@ T& StackExt<T>::findMin() {
     T& result = this->minimums.top();
     return result;
 }
+
+int getWords (const string &phrase) {
+    int result = 0;
+    for (auto letter : phrase) if (letter == ' ') result++;
+    return phrase.size()? result + 1 : 0;
+}
+
+string getLoser (int words, list<string> kids) {
+
+    int index = 0;
+
+    while (kids.size() > 1) {
+        index = (words-1+index) % kids.size();
+        list<string>::iterator it = kids.begin();
+        for (int i = 0 ; i < index ; i++) { it++; }
+        kids.remove(*it);
+    }
+
+    return kids.front();
+}
+
+void showResults (const string& phrase, const list<string> &kids) {
+
+    string loser = getLoser(getWords(phrase), kids);
+    if (loser == "Carlos") {
+        cout << "O Carlos nao se livrou" << endl;
+    } else {
+        cout << "O Carlos livrou-se (coitado do " << loser << "!)" << endl;
+    }
+}
+
+int main () {
+
+    list<string> names = {};
+    int loops, nNames;
+    string phrase, currentName;
+    cin >> loops;
+
+    for (int i = 0 ; i < loops ; i++) {
+        cin.clear();
+        cin.ignore(10000, '\n');
+        getline(cin, phrase);
+        cin >> nNames;
+        for (int j = 0 ; j < nNames ; j++) {
+            cin >> currentName;
+            names.push_back(currentName);
+        }
+        showResults(phrase, names);
+        names.clear();
+    }
+
+    return 0;
+}
+
