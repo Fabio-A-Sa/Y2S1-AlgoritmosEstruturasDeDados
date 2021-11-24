dc381f0c050f0f983c371b42d6031fabd16b0dee
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-24 00:23:13 +0000

    Init

--- Mooshak/Semana #3/Confusão no Aeroporto.cpp
+++ Mooshak/Semana #3/Confusão no Aeroporto.cpp
@@ -1,4 +1,33 @@
-//
-// Created by Fábio Sá on 24/11/2021.
-//
+// Created on 24 November, 2021
+// author: Fábio Araújo de Sá, up202007658
 
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Plane {
+
+    private string name;
+    private int minute;
+    private int delay;
+
+    public:
+        Plane (string name, int minute) : name(name), minute(minute), delay(0) {}
+        void setDelay(int delay) { this->delay = delay; }
+        int getDelay() { return delay; }
+        int getMinute() { return minute; }
+        string getName() { return name; }
+        ostream & operator << (ostream & os, const Plane &p);
+};
+
+ostream & Plane::operator << (ostream & os, const Plane &p) {
+    os << p.getName() << " " << p.getDelay() << endl;
+    return os;
+}
+
+int main () {
+
+
+    return 0;
+}
