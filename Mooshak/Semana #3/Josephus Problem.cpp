e19b8d5fa68aab2169adff277c5d4e7883287008
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-24 17:56:05 +0000

    Inputs

--- Mooshak/Semana #3/Josephus Problem.cpp
+++ Mooshak/Semana #3/Josephus Problem.cpp
@@ -1,4 +1,19 @@
-//
-// Created by Fábio Sá on 24/11/2021.
-//
+// Created on 24 November, 2021
+// author: Fábio Araújo de Sá, up202007658
 
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void solve (int a, int b, int c) {
+    cout << "Test: " << a << " " << b << " " << c << endl;
+}
+
+int main () {
+
+    int n1, n2, n3;
+    cin >> n1 >> n2 >> n3;
+    solve (n1, n2, n3);
+    return 0;
+}
