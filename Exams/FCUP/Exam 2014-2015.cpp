fe96ee73d60dc336e2c48fc423f02f01449862ab
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-08 12:22:14 +0000

    Rotate

--- Exams/FCUP/Exam 2014-2015.cpp
+++ Exams/FCUP/Exam 2014-2015.cpp
@@ -32,10 +32,14 @@ class Rectangle {
             this->x1 = -abs(x1-x0);
             this->y1 = abs(y1-y0);
         }
+
+        int compareTo(Rectangle r) {
+            if (this->x0 < r.x0 && this->x1 < r.x1) return 0;
+            else if (this->y1 < r.y1 && this->y0 < r.y0) return -1;
+            return 1;
+        }
 };
 
 int main () {
-
-
     return 0;
 }
