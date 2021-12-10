7f2fbc9d8f1c5d736dcd4f935b05ad6676aab1da
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-10 23:13:43 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -3775,7 +3775,6 @@ public:
     void incNVisits();
 };
 
-
 class Game {
     BinaryTree<Circle> game;
 public:
@@ -3784,4 +3783,95 @@ public:
     int play();
     int mostVisited() const;
 };
+
+using namespace std;
+
+WordMean::WordMean(string w, string m): word(w), meaning(m) {}
+
+string WordMean::getWord() const {
+    return word;
+}
+
+string WordMean::getMeaning() const {
+    return meaning;
+}
+
+void WordMean::setMeaning(string m) {
+    meaning = m;
+}
+
+void WordMean::setWord(string w) {
+    word = w;
+}
+
+BST<WordMean> Dictionary::getWords() const {
+    return words;
+}
+
+bool WordMean::operator < (const WordMean &w) const {
+    return this->getWord() < w.getWord();
+}
+
+bool WordMean::operator == (const WordMean &a) const {
+    return this->getWord() == a.getWord();
+}
+
+void Dictionary::readFile(ifstream &f) {
+
+    WordMean notFound = WordMean("", "");
+    string currentWord, currentMeaning;
+
+    while (!f.eof()) {
+        getline(f, currentWord);
+        getline(f, currentMeaning);
+        WordMean current = WordMean(currentWord, currentMeaning);
+        WordMean found = words.find(current);
+        if (found == notFound) words.insert(current);
+    }
+}
+
+string Dictionary::consult(string word1, WordMean& previous, WordMean& next) const {
+
+    WordMean toSearch = WordMean(word1, "");
+    WordMean notFound = WordMean("", "");
+    WordMean found = words.find(toSearch);
+    if (found == notFound) {
+
+        BSTItrIn<WordMean> itr(words);
+
+        while (previous.getWord() > word1 || next.getWord() < word1) {
+            previous = next;
+            itr.advance();
+            next = itr.retrieve();
+        }
+
+        return "word not found";
+
+    } else {
+        return found.getMeaning();
+    }
+}
+
+bool Dictionary::update(string word1, string mean1) {
+
+    WordMean found = words.find(WordMean(word1, ""));
+
+    if (found == WordMean("", "")) {
+        words.insert(WordMean(word1, mean1));
+    } else {
+        words.remove(found);
+        words.insert(WordMean(word1, mean1));
+    }
+    return !(found == WordMean("", ""));
+}
+
+void Dictionary::print() const {
+
+    BSTItrIn<WordMean> itr(words);
+    while (!itr.isAtEnd()) {
+        cout << itr.retrieve().getWord() << endl;
+        cout << itr.retrieve().getMeaning() << endl;
+        itr.advance();
+    }
+}
 
