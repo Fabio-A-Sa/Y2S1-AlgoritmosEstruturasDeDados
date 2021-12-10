a992596aa2a4ef213c87839cb982de4fddca197f
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-10 23:12:43 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -3736,3 +3736,29 @@ void BTItrLevel<T>::advance() {
     if (next)
         itrQueue.push(next);
 }
+
+class WordMean {
+    string word;
+    string meaning;
+public:
+    WordMean(string w, string m);
+    string getWord() const;
+    string getMeaning() const;
+    void setMeaning(string m);
+    void setWord(string w);
+    bool operator<(const WordMean &w) const;
+    bool operator==(const WordMean &w) const;
+};
+
+class Dictionary
+{
+    BST<WordMean> words;
+public:
+    Dictionary(): words(WordMean("","")) {};
+    BST<WordMean> getWords() const;
+    void readFile(ifstream& f);
+    string consult(string word1, WordMean& previous, WordMean& next) const;
+    bool update(string word1, string mean1);
+    void print() const;
+};
+
