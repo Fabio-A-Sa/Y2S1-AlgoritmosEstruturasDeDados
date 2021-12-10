2c0b7ec89d6a2c0b0fc438d63606c0b0857bc81b
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-10 23:14:05 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -3875,3 +3875,100 @@ void Dictionary::print() const {
     }
 }
 
+Circle::Circle(int p, bool s): points(p), state(s), nVisits(0) {}
+
+int Circle::getPoints() const {
+    return points;
+}
+
+bool Circle::getState() const {
+    return state;
+}
+
+void Circle::changeState() {
+    if (state == false)
+        state = true;
+    else
+        state = false;
+}
+
+int Circle::getNVisits() const {
+    return nVisits;
+}
+
+void Circle::incNVisits() {
+    nVisits++;
+}
+
+
+BinaryTree<Circle>& Game::getGame() {
+    return game;
+}
+
+// Aided by Pedro Barbeira
+Game::Game(int height, vector<int>& points, vector<bool>& states) {
+
+    vector<BinaryTree<Circle>*> leaves(points.size(), nullptr);
+    for(int i = points.size()-1; i >= 0; i--){
+        if(i*2+1 > points.size()-1){
+            leaves[i] = new BinaryTree<Circle>(Circle(points[i], states[i]));
+        }
+        else{
+            leaves[i] = new BinaryTree<Circle>(Circle(points[i], states[i]), (*leaves[2*i+1]), (*leaves[2*i+2]));
+        }
+    }
+    game = (*leaves[0]);
+    for(auto it: leaves)
+        delete it;
+}
+
+// Aided by Pedro Barbeira
+int Game::play() {
+
+    BTItrLevel<Circle> it(game);
+    Circle c = it.retrieve();
+    bool over = false;
+    while(!it.isAtEnd()){
+        Circle& current = it.retrieve();
+        c.incNVisits();
+        if(c.getState()){
+            for(int p = c.getPoints(); p >= 0; p--) {
+                it.advance();
+                if (it.isAtEnd()) {
+                    over = true;
+                    break;
+                }
+            }
+        }
+        else{
+            for(int p = c.getPoints(); p > 0; p--){
+                it.advance();
+                if (it.isAtEnd()) {
+                    over = true;
+                    break;
+                }
+            }
+        }
+        if(over) break;
+        else {
+            c.changeState();
+            current = c;
+            c = it.retrieve();
+        }
+    }
+    return c.getPoints();
+}
+
+int Game::mostVisited() const {
+
+    BTItrLevel<Circle> it(game);
+    it.advance();
+    int max = 0;
+    while(!it.isAtEnd()){
+        Circle c = it.retrieve();
+        if(c.getNVisits() > max) max = c.getNVisits();
+        it.advance();
+    }
+    return max;
+}
+
