3b0023fe0eaad69ce8db2c4477413b51c6d310f4
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-10 00:19:31 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -2857,3 +2857,32 @@ void minSelectionSort (int[] values) {
 int main () {
     return 0;
 }
+
+class Matrix {
+
+    int rows;
+    int columns;
+    vector<vector<int>> matrix;
+
+public:
+
+    Matrix(int rows, int columns, int valueK) {
+        this->rows = rows;
+        this->columns = columns;
+        this->matrix = vector<vector<int>(rows, k)>(columns);
+    }
+
+};
+
+class C1 {
+
+    int value;
+
+public:
+    C1(int value);
+};
+
+int main () {
+    C1 c;
+    return 0;
+}
