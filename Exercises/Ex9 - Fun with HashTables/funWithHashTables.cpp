03d9d451f64c84f8ae39bb65c0420f324d1c12bf
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-22 23:16:29 +0000

    Normalization

--- Exercises/Ex9 - Fun with HashTables/funWithHashTables.cpp
+++ Exercises/Ex9 - Fun with HashTables/funWithHashTables.cpp
@@ -1,5 +1,5 @@
-// AED 2021/2022 - Aula Pratica 08
-// Pedro Ribeiro (DCC/FCUP) [12/12/2021]
+// Created on December, 2021
+// @author: Fábio Araújo de Sá
 
 #include "funWithHashTables.h"
 #include "hashTable.h"
@@ -8,32 +8,19 @@
 #include <iostream>
 #include <cctype>
 
-// Exemplo de função de hash para ints
 unsigned FunWithHashTables::hashInt(const int& i) {
   return i;
 }
 
-// Exemplo de função de hash para strings
 unsigned FunWithHashTables::hashString(const string& s) {
   hash<string> h;
   return h(s);
 }
 
-// ----------------------------------------------------------
-// Exercicio 1: Tabelas de Dispersão
-// ----------------------------------------------------------
-
-// ..............................
-// d) Contando diferentes somas de pares
-// TODO
 int FunWithHashTables::sumPairs(const vector<int>& numbers) {
   return 0;
 }
 
-// ----------------------------------------------------------
-// Exercicio 2: Um padrão no DNA
-// ----------------------------------------------------------
-// TODO
 int FunWithHashTables::dnaMotifs(string dna, int k, unordered_set<string>& motifs) {
   return 0;
 }
