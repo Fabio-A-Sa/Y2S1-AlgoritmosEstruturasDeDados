ce57615985783a3d50db88c3abee8bdf3ecb3a04
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-22 18:37:07 +0000

    Exercise 1

--- Exercises/Ex8 - Fun with Trees/funWithBSTs.cpp
+++ Exercises/Ex8 - Fun with Trees/funWithBSTs.cpp
@@ -4,7 +4,14 @@
 #include "funWithBSTs.h"
 
 int FunWithBSTs::newBag(const vector<int>& collection, const vector<int>& bag) {
-  return 0;
+
+    set<int> unRepeted;
+    for (int cromo : collection) unRepeted.insert(cromo);
+    int beforeInsertion = unRepeted.size();
+
+    for (int newCromo : bag) unRepeted.insert(newCromo);
+
+    return unRepeted.size() - beforeInsertion;
 }
 
 int FunWithBSTs::battle(const vector<int>& alice, const vector<int>& bruno) {
