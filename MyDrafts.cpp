962df16e8e0ea1eafbea8ce11e35c9ed56d244e1
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-22 18:43:16 +0000

    MyDrafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -4033,3 +4033,33 @@ int FunWithBSTs::newBag(const vector<int>& collection, const vector<int>& bag) {
 
     return unRepeted.size() - beforeInsertion;
 }
+
+int FunWithBSTs::battle(const vector<int>& alice, const vector<int>& bruno) {
+
+    multiset<int> mAlice; for (int card : alice) mAlice.insert(card);
+    multiset<int> mBruno; for (int card : bruno) mBruno.insert(card);
+
+    multiset<int>::iterator iteratorAlice;
+    multiset<int>::iterator iteratorBruno;
+
+    while (!mAlice.empty() && !mBruno.empty()) {
+
+        iteratorAlice = mAlice.end(); iteratorAlice--;
+        iteratorBruno = mBruno.end(); iteratorBruno--;
+
+        int maxAlice = *iteratorAlice; mAlice.erase(iteratorAlice);
+        int maxBruno = *iteratorBruno; mBruno.erase(iteratorBruno);
+
+        if (maxAlice > maxBruno) {
+            maxAlice -= maxBruno;
+            mAlice.insert(maxAlice);
+        } else if (maxAlice < maxBruno) {
+            maxBruno -= maxAlice;
+            mBruno.insert(maxBruno);
+        } else continue;
+    }
+
+    if (!mAlice.empty()) return mAlice.size();
+    if (!mBruno.empty()) return -mBruno.size();
+    return 0;
+}
