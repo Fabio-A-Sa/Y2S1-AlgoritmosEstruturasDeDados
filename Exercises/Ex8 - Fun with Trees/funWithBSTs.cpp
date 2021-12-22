c35a6a4db113bc68733d633d4efa47e5386e786b
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-22 18:36:01 +0000

    Normalization

--- Exercises/Ex8 - Fun with Trees/funWithBSTs.cpp
+++ Exercises/Ex8 - Fun with Trees/funWithBSTs.cpp
@@ -4,42 +4,21 @@
 #include "funWithBSTs.h"
 #include "bst.h"
 
-// ----------------------------------------------------------
-// Exercicio 1: Colecao de Cromos
-// ----------------------------------------------------------
-// TODO
 int FunWithBSTs::newBag(const vector<int>& collection, const vector<int>& bag) {
   return 0;
 }
 
-// ----------------------------------------------------------
-// Exercicio 2: Batalha de Pokemons
-// ----------------------------------------------------------
-// TODO
 int FunWithBSTs::battle(const vector<int>& alice, const vector<int>& bruno) {
   return 0;
 }
 
-// ----------------------------------------------------------
-// Exercicio 3: Reviews Cinematograficas
-// ----------------------------------------------------------
-
-// ..............................
-// a) Contando Filmes
-// TODO
 int FunWithBSTs::numberMovies(const vector<pair<string, int>>& reviews) {
   return 0;
 }
 
-// ..............................
-// b) O filme com mais reviews
-// TODO
 void FunWithBSTs::moreReviews(const vector<pair<string, int>>& reviews, int& m, int& n) {
 }
 
-// ..............................
-// c) Os melhores filmes
-// TODO
 vector<string> FunWithBSTs::topMovies(const vector<pair<string, int>>& reviews, double k) {
     vector<string> answer;
     return answer;
