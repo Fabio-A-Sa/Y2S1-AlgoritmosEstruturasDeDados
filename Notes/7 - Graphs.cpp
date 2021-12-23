18206c4881e7a6564b6bb140bd83b0eb958b9403
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-12-23 23:37:21 +0000

    some drafts

--- Notes/7 - Graphs.cpp
+++ Notes/7 - Graphs.cpp
@@ -10,13 +10,18 @@ using namespace std;
 class Graph {
 
     /**
-     * 
+     * Uma struct que implementa uma aresta entre dois nós.
+     * Destino, se for um grafo direcional. Por predefinição, implementa-se uma bidirecional A -> B e B -> A
+     * Peso, se for um grafo pesado. Por predefinição, implementa-se weight = 1
      */
     struct Edge {
         int destination;
         int weight;
     };
 
+    /**
+     * 
+     */
     struct Node {
         list<Edge> adjacents;
     };
