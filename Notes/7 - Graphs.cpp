84c87e24f859daf0fb687e71dbf0f89310d872ea
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-12-23 23:27:42 +0000

    added structs

--- Notes/7 - Graphs.cpp
+++ Notes/7 - Graphs.cpp
@@ -2,12 +2,28 @@
 // @author: Fábio Araújo de Sá
 
 #include <iostream>
+#include <string>
+#include <vector>
 #include <list>
 using namespace std;
 
-
 class Graph {
-    
+
+    /**
+     * 
+     */
+    struct Edge {
+        int destination;
+        int weight;
+    };
+
+    struct Node {
+        list<Edge> adjacents;
+    };
+
+    int size;
+    bool hasDirection;
+    vector<Node> nodes;
 };
 
 int main () {
