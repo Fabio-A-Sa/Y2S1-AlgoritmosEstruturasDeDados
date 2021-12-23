d07c1d776b28f6607fa115dba290eb3acace24f5
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-12-23 18:31:03 +0000

    little modifications

---
+++ Notes/7 - Graphs.cpp
@@ -0,0 +1,11 @@
+// Created on December, 2021
+// @author: Fábio Araújo de Sá
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main () {
+    cout << "Hello world" << endl;
+    return 0;
+}
