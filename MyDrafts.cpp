f8cbfc5e32537337b2f8b5d7113176e85d628689
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-12-24 00:34:13 +0000

    drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -4409,3 +4409,85 @@ bool isValid2 (string text) {
         return (text[0] == 1 || text[0] == 0) && isValid2(text.substr(1, text.size()-1));
     }
 }
+
+class Graph {
+
+private:
+    /**
+     * Uma struct que implementa uma aresta entre dois nós.
+     * Destino, se for um grafo direcional. Por predefinição, implementa-se uma bidirecional A -> B e B -> A
+     * Peso, se for um grafo pesado. Por predefinição, implementa-se weight = 1
+     */
+    struct Edge {
+        int destination;
+        int weight;
+    };
+
+    /**
+     * Um nó é representado por uma lista de arestas adjacentes, que podem ou não ter
+     * direção ou peso
+     */
+    struct Node {
+        list<Edge> adjacents;
+    };
+
+    /**
+     * Tamanho do grafo
+     */
+    int size;
+
+    /**
+     * Se ele tem direção
+     */
+    bool hasDirection;
+
+    /**
+     * Um grafo é constituido por um vector de nós
+     */
+    vector<Node> nodes;
+
+public:
+
+    /**
+     * Construtor de um grafo com base no tamanho dos nós a albergar. Por predefinição, é um grafo não direcional
+     */
+    Graph(int nodes, bool dir = false);
+
+    /**
+     * Adiciona uma ligação entre a source e o destino, com ou sem peso associado
+     */
+    void addEdge (int source, int destination, int weight = 1);
+};
+
+Graph::Graph(int nodes, bool dir) : nodes(nodes+1) {
+    this->size = nodes;
+    this->hasDirection= dir;
+}
+
+void Graph::addEdge(int source, int destination, int weight) {
+    if (source < 1 || source > size || destination < 1 || destination > size) return;
+    Edge newEdge;
+    newEdge.destination = destination;
+    newEdge.weight = weight;
+    nodes[source].adjacents.push_back(newEdge);
+    if (!hasDirection) {
+        newEdge.destination = destination;
+        newEdge.weight = weight;
+        nodes[destination].adjacents.push_back(newEdge);
+    }
+}
+
+int main () {
+
+    Graph graph(9, false);
+    graph.addEdge(1, 2);
+    graph.addEdge(1, 3);
+    graph.addEdge(1, 4);
+    graph.addEdge(2, 4);
+    graph.addEdge(3, 4);
+    graph.addEdge(4, 5);
+    graph.addEdge(4, 6);
+    graph.addEdge(4, 7);
+    graph.addEdge(5, 8);
+    graph.addEdge(7, 9);
+}
