62a0d72d5f5efd745c835c4a030f0954da3d697f
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-09 17:03:27 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -5002,60 +5002,74 @@ Graph FunWithGraphs::graph8() {
     Graph g(49, false);
 
     // F
-    g.addEdge(1,2);
-    g.addEdge(1,23);
-    g.addEdge(23,24);
-    g.addEdge(23,29);
+    g.addEdge(1, 2);
+    g.addEdge(1, 23);
+    g.addEdge(23, 24);
+    g.addEdge(23, 29);
     // E
-    g.addEdge(3,4);
-    g.addEdge(3,25);
-    g.addEdge(25,26);
-    g.addEdge(25,30);
-    g.addEdge(30,31);
+    g.addEdge(3, 4);
+    g.addEdge(3, 25);
+    g.addEdge(25, 26);
+    g.addEdge(25, 30);
+    g.addEdge(30, 31);
     // L
-    g.addEdge(5,32);
-    g.addEdge(5,33);
+    g.addEdge(5, 32);
+    g.addEdge(5, 33);
     // I
-    g.addEdge(6,34);
+    g.addEdge(6, 34);
     // Z
-    g.addEdge(7,8);
-    g.addEdge(7,35);
-    g.addEdge(35,36);
-    g.addEdge(3,25);
+    g.addEdge(7, 8);
+    g.addEdge(7, 35);
+    g.addEdge(35, 36);
+    g.addEdge(3, 25);
 
     // A
-    g.addEdge(9,10);
-    g.addEdge(9,27);
-    g.addEdge(27,28);
-    g.addEdge(10,28);
-    g.addEdge(27,37);
-    g.addEdge(28,38);
+    g.addEdge(9, 10);
+    g.addEdge(9, 27);
+    g.addEdge(27, 28);
+    g.addEdge(10, 28);
+    g.addEdge(27, 37);
+    g.addEdge(28, 38);
     // N
-    g.addEdge(11,39);
-    g.addEdge(11,40);
-    g.addEdge(12,40);
+    g.addEdge(11, 39);
+    g.addEdge(11, 40);
+    g.addEdge(12, 40);
     // O
-    g.addEdge(13,14);
-    g.addEdge(13,41);
-    g.addEdge(41,42);
-    g.addEdge(14,42);
+    g.addEdge(13, 14);
+    g.addEdge(13, 41);
+    g.addEdge(41, 42);
+    g.addEdge(14, 42);
 
     // N
-    g.addEdge(15,43);
-    g.addEdge(15,44);
-    g.addEdge(16,44);
+    g.addEdge(15, 43);
+    g.addEdge(15, 44);
+    g.addEdge(16, 44);
     // O
-    g.addEdge(17,18);
-    g.addEdge(17,45);
-    g.addEdge(45,46);
-    g.addEdge(18,46);
+    g.addEdge(17, 18);
+    g.addEdge(17, 45);
+    g.addEdge(45, 46);
+    g.addEdge(18, 46);
     // V
-    g.addEdge(19,47);
-    g.addEdge(20,47);
+    g.addEdge(19, 47);
+    g.addEdge(20, 47);
     // O
-    g.addEdge(21,22);
-    g.addEdge(21,48);
-    g.addEdge(48,49);
-    g.addEdge(22,49);
+    g.addEdge(21, 22);
+    g.addEdge(21, 48);
+    g.addEdge(48, 49);
+    g.addEdge(22, 49);
 
     return g;
+}
+
+class FunWithGraphs {
+public:
+
+    static Graph graph1();
+    static Graph graph2();
+    static Graph graph3();
+    static Graph graph4();
+    static Graph graph5();
+    static Graph graph6();
+    static Graph graph7();
+    static Graph graph8();
+};
