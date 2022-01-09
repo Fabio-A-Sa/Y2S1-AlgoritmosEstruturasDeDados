47568ed2a0c15a61668a42882ec36cb401e66d4d
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-09 17:04:20 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -5126,3 +5126,39 @@ public:
     bool colorsDFS(int v);
     void resetNodes();
 };
+
+Graph::Graph(int num, bool dir) : n(num), hasDir(dir), nodes(num+1) {}
+
+void Graph::addEdge(int src, int dest, int weight) {
+    if (src<1 || src>n || dest<1 || dest>n) return;
+    nodes[src].adj.push_back({dest, weight});
+    if (!hasDir) nodes[dest].adj.push_back({src, weight});
+}
+
+void Graph::dfs(int v) {
+
+    nodes[v].visited = true;
+    for (auto e : nodes[v].adj) {
+        int w = e.dest;
+        if (!nodes[w].visited)
+            dfs(w);
+    }
+}
+
+void Graph::bfs(int v) {
+
+    for (int v=1; v<=n; v++) nodes[v].visited = false;
+    queue<int> q;
+    q.push(v);
+    nodes[v]. visited = true;
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        for (auto e : nodes[u].adj) {
+            int w = e.dest;
+            if (!nodes[w].visited) {
+                q.push(w);
+                nodes[w].visited = true;
+            }
+        }
+    }
+}
