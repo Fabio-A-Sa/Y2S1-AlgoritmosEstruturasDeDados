3c10ea46ebce828a4e134da6c2d062ce333456c8
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-09 16:55:02 +0000

    normalization

--- Exercises/Ex10 - Fun with Graphs/graph.cpp
+++ Exercises/Ex10 - Fun with Graphs/graph.cpp
@@ -3,20 +3,16 @@
 
 #include "graph.h"
 
-// Constructor: nr nodes and direction (default: undirected)
-Graph::Graph(int num, bool dir) : n(num), hasDir(dir), nodes(num+1) {
-}
+Graph::Graph(int num, bool dir) : n(num), hasDir(dir), nodes(num+1) {}
 
-// Add edge from source to destination with a certain weight
 void Graph::addEdge(int src, int dest, int weight) {
     if (src<1 || src>n || dest<1 || dest>n) return;
     nodes[src].adj.push_back({dest, weight});
     if (!hasDir) nodes[dest].adj.push_back({src, weight});
 }
 
-// Depth-First Search: example implementation
 void Graph::dfs(int v) {
-    cout << v << " "; // show node order
+
     nodes[v].visited = true;
     for (auto e : nodes[v].adj) {
         int w = e.dest;
@@ -25,15 +21,14 @@ void Graph::dfs(int v) {
     }
 }
 
-// Depth-First Search: example implementation
 void Graph::bfs(int v) {
+
     for (int v=1; v<=n; v++) nodes[v].visited = false;
-    queue<int> q; // queue of unvisited nodes
+    queue<int> q;
     q.push(v);
     nodes[v]. visited = true;
-    while (!q.empty()) { // while there are still unvisited nodes
+    while (!q.empty()) {
         int u = q.front(); q.pop();
-        cout << u << " "; // show node order
         for (auto e : nodes[u].adj) {
             int w = e.dest;
             if (!nodes[w].visited) {
@@ -44,67 +39,31 @@ void Graph::bfs(int v) {
     }
 }
 
-// ----------------------------------------------------------
-// Exercicio 1: Introdução a uma classe simplificada de grafos
-// ----------------------------------------------------------
-
-// ..............................
-// a) Contando diferentes somas de pares
-// TODO
 int Graph::outDegree(int v) {
     return 0;
 }
 
-// ----------------------------------------------------------
-// Exercicio 2: Componentes conexos
-// ----------------------------------------------------------
-
-// ..............................
-// a) Contando componentes conexos
-// TODO
 int Graph::connectedComponents() {
     return 0;
 }
 
-// ..............................
-// b) Componente gigante
-// TODO
 int Graph::giantComponent() {
     return 0;
 }
 
-
-// ----------------------------------------------------------
-// Exercicio 3: Ordenacao topologica
-// ----------------------------------------------------------
-// TODO
 list<int> Graph::topologicalSorting() {
     list<int> order;
     return order;
 }
 
-// ----------------------------------------------------------
-// Exercicio 4: Distancias em grafos nao pesados
-// ----------------------------------------------------------
-
-// ..............................
-// a) Distancia entre dois nos
-// TODO
 int Graph::distance(int a, int b) {
     return 0;
 }
 
-// ..............................
-// b) Diametro
-// TODO
 int Graph::diameter() {
     return 0;
 }
 
-// ----------------------------------------------------------
-// Exercicio 5: To or not be… a DAG!
-// ----------------------------------------------------------
-// TODO
 bool Graph::hasCycle() {
     return false;
 }
