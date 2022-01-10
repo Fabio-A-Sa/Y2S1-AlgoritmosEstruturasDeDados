d698dceefb640ed73ac039d6c94ada45b291e50b
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-10 23:36:29 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -5664,3 +5664,71 @@ int main () {
     heapSort();
     return 0;
 }
+
+// @Overload
+void showContent(const vector<int> &numbers) {
+    for (int number : numbers) cout << number << " ";
+    cout << endl;
+}
+
+// @Overload
+void showContent(priority_queue<int> numbers) {
+    while (!numbers.empty()) {
+        cout << numbers.top() << " ";
+        numbers.pop();
+    }
+    cout << endl;
+}
+
+vector<int> getRandomNumbers() {
+
+    vector<int> numbers = {};
+    for (int i = 0 ; i < 20 ; i++) numbers.push_back(1 + rand() % 100);
+    return numbers;
+}
+
+void priorityQueue() {
+
+    vector<int> numbers = getRandomNumbers();
+    cout << "\nInitial content: " << endl;
+    showContent(numbers);
+
+    priority_queue<int> sortedNumbers;
+    for (int number : numbers) sortedNumbers.push(number);
+
+    cout << "\nSorted content: " << endl;
+    showContent(sortedNumbers);
+}
+
+void verify(const vector<int> &numbers)  {
+
+    cout << "\nVerification:\n" << endl;
+
+    map<int, int> indexNumber = {};
+    for (int i = 0 ; i < numbers.size() ; i++) {
+        indexNumber.insert(make_pair(i+1, numbers[i]));
+    }
+
+    for (int i = 0 ; i < numbers.size() ; i++) {
+        int index1 = (i+1)*2;
+        int index2 = (i+1)*2+1;
+        if (index2 < numbers.size()) {
+            cout << "Parent: " << numbers[i] << endl;
+            cout << "Child 1: " << numbers[index1-1] << " correct? " << (numbers[index1-1] == indexNumber[index1]) << endl;
+            cout << "Child 2: " << numbers[index2-1] << " correct? " << (numbers[index2-1] == indexNumber[index2]) << endl;
+            cout << endl;
+        }
+    }
+}
+
+void heapSort() {
+
+    vector<int> numbers = getRandomNumbers();
+    cout << "\nInitial content: " << endl;
+    showContent(numbers);
+
+    make_heap(numbers.begin(), numbers.end());
+    cout << "\nSorted content: " << endl;
+    showContent(numbers);
+    verify(numbers);
+}
