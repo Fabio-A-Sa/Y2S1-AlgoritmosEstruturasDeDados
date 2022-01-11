bfdc35ab985ed5865c1d87e73bbe387b377fa714
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-11 22:46:54 +0000

    Exercise 4

--- Exercises/Ex11 - Minimal Spanning Trees/disjointSets.h
+++ Exercises/Ex11 - Minimal Spanning Trees/disjointSets.h
@@ -30,21 +30,34 @@ class DisjointSets {
 template <class T>
 void DisjointSets<T>::makeSet(const T& x) {
     a[x].parent = x;
+    a[x].myrank = 0;
 }
 
 // Find the representative of the set of element x
 template <class T>
 T DisjointSets<T>::find(const T& x) {
-    if (a[x].parent == x) return x;
-    else return find(a[x].parent);
+    if (a[x].parent != x)
+        a[x].parent = find(a[x].parent);
+    return a[x].parent;
 }
 
 // Merge the sets of elements x and y
 template <class T>
 void DisjointSets<T>::unite(const T& x, const T& y) {
+
     T xRoot = find(x);
     T yRoot = find(y);
-    a[yRoot].parent = xRoot;
+
+    if (a[xRoot].myrank < a[yRoot].myrank) {
+        a[xRoot].parent = yRoot;
+
+    } else if (a[xRoot].myrank > a[yRoot].myrank) {
+        a[yRoot].parent = xRoot;
+
+    } else {
+        a[yRoot].parent = xRoot;
+        a[xRoot].myrank = a[xRoot].myrank + 1;
+    }
 }
 
 #endif
