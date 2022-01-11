cd378cfc188b90f72c0f191a626fdbe8047f8ab6
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-11 22:39:11 +0000

    normalizationo

--- Exercises/Ex11 - Minimal Spanning Trees/disjointSets.h
+++ Exercises/Ex11 - Minimal Spanning Trees/disjointSets.h
@@ -1,5 +1,5 @@
-// AED 2021/2022 - Aula Pratica 10
-// Pedro Ribeiro (DCC/FCUP) [09/01/2022]
+// Created on January, 2022
+// @author: Fábio Araújo de Sá
 
 #ifndef _DISJOINTSETS_H_
 #define _DISJOINTSETS_H_
@@ -8,17 +8,21 @@
 
 template <class T>
 class DisjointSets {
-    struct Node {
-        T parent;
-        int myrank; // to use on union by rank
-    };
 
-    unordered_map<T, Node> a;
+    private:
 
-public:
-    void makeSet(const T& x);            // Create a set with a single element x
-    T find(const T& x);                  // Find the representative of the set of element x
-    void unite(const T& x, const T& y);  // Merge the sets of elements x and y
+        struct Node {
+            T parent;
+            int myrank; // to use on union by rank
+        };
+
+        unordered_map<T, Node> a;
+
+    public:
+
+        void makeSet(const T& x);            // Create a set with a single element x
+        T find(const T& x);                  // Find the representative of the set of element x
+        void unite(const T& x, const T& y);  // Merge the sets of elements x and y
 
 };
 
