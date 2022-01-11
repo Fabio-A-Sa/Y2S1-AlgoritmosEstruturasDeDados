0ace4af0ffeb4b4a0ea511a8376fb2044ecac142
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-11 22:53:19 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -6095,3 +6095,108 @@ public:
     static int gridCity(const vector<pair<int, int>>& plants, const vector<pair<int, int>>& houses);
 };
 
+Graph::Graph(int num, bool dir) : n(num), hasDir(dir), nodes(num+1) {}
+
+void Graph::addEdge(int src, int dest, int weight) {
+    if (src<1 || src>n || dest<1 || dest>n) return;
+    nodes[src].adj.push_back({dest, weight});
+    if (!hasDir) nodes[dest].adj.push_back({src, weight});
+}
+
+int Graph::prim(int r) {
+
+    MinHeap<int, int> heap(n, -1);
+
+    for (int i = 1 ; i <= n ; i++) {
+        nodes[i].distance = INT_MAX;
+        nodes[i].parent = NULL;
+        heap.insert(i, nodes[i].distance);
+    }
+
+    nodes[r].distance = 0;
+    heap.decreaseKey(r, 0);
+
+    while (heap.getSize()) {
+
+        int nodeIndex = heap.removeMin(); // o índice do nó que possui menor distância a r
+
+        for (Edge edge : nodes[nodeIndex].adj) {
+            int neighbor = edge.dest;
+            if (heap.hasKey(neighbor) && edge.weight < nodes[neighbor].distance) {
+                nodes[neighbor].parent = neighbor;
+                nodes[neighbor].distance = edge.weight;
+                heap.decreaseKey(neighbor, edge.weight);
+            }
+        }
+    }
+
+    int total = 0;
+    for (int i = 1 ; i <= n ; i++) {
+        total += nodes[i].distance;
+        cout << "Node " << i << " distance: " << nodes[i].distance << endl;
+    }
+
+    return total;
+}
+
+int Graph::kruskal() {
+
+    vector<Edge> minimalSpanningTree = {};
+    vector<Edge> allEdges = {};
+    DisjointSets<int> set;
+
+    for (int i = 0 ; i <= n ; i++) {
+        set.makeSet(i);
+        for (Edge edge : nodes[i].adj) {
+            edge.origin = i;
+            allEdges.push_back(edge);
+        }
+    }
+
+    sort(allEdges.begin(), allEdges.end(), [](const Edge &e1, const Edge &e2) { return e1.weight < e2.weight; });
+
+    for (Edge edge : allEdges) {
+        if (set.find(edge.origin) != set.find(edge.dest)) {
+            minimalSpanningTree.push_back(edge);
+            set.unite(edge.origin, edge.dest);
+        }
+    }
+
+    int total = 0;
+    for (Edge edge : minimalSpanningTree)
+        total += edge.weight;
+    return total;
+}
+
+class Graph {
+
+private:
+
+    struct Edge {
+        int origin; // origin node
+        int dest;   // Destination node
+        int weight; // An integer weight
+    };
+
+    struct Node {
+        list<Edge> adj; // The list of outgoing edges (to adjacent nodes)
+        int distance;
+        int parent;
+    };
+
+    int n;              // Graph size (vertices are numbered from 1 to n)
+    bool hasDir;        // false: undirect; true: directed
+    vector<Node> nodes; // The list of nodes being represented
+
+public:
+
+    // Constructor: nr nodes and direction (default: undirected)
+    Graph(int nodes, bool dir = false);
+
+    // Add edge from source to destination with a certain weight
+    void addEdge(int src, int dest, int weight = 1);
+
+    // ----- Functions to implement in this class -----
+    int prim(int v);
+    int kruskal();
+};
