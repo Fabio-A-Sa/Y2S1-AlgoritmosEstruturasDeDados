f68fd3eea59f0266e4f5b01f0ae5daa0ef7eac65
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-11 22:53:03 +0000

    Drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -6080,3 +6080,18 @@ int FunWithGraphs::game(const vector<int>& v) {
 int FunWithGraphs::gridCity(const vector<pair<int, int>>& plants, const vector<pair<int, int>>& houses) {
     return 0;
 }
+
+class FunWithGraphs {
+
+public:
+    // Some example graphs
+    static Graph graph1();
+    static Graph graph2();
+    static Graph graph3();
+    static Graph graph4();
+
+    // ----- Functions to implement in this class -----
+    static int game(const vector<int>& v);
+    static int gridCity(const vector<pair<int, int>>& plants, const vector<pair<int, int>>& houses);
+};
+
