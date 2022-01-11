e68ebcbae94be72243b38bcb411f5e1a7307403b
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-11 22:44:45 +0000

    Exercise 3

--- Exercises/Ex11 - Minimal Spanning Trees/graph.cpp
+++ Exercises/Ex11 - Minimal Spanning Trees/graph.cpp
@@ -12,7 +12,39 @@ void Graph::addEdge(int src, int dest, int weight) {
 }
 
 int Graph::prim(int r) {
-    return 0;
+
+    MinHeap<int, int> heap(n, -1);
+
+    for (int i = 1 ; i <= n ; i++) {
+        nodes[i].distance = INT_MAX;
+        nodes[i].parent = NULL;
+        heap.insert(i, nodes[i].distance);
+    }
+
+    nodes[r].distance = 0;
+    heap.decreaseKey(r, 0);
+
+    while (heap.getSize()) {
+
+        int nodeIndex = heap.removeMin(); // o índice do nó que possui menor distância a r
+
+        for (Edge edge : nodes[nodeIndex].adj) {
+            int neighbor = edge.dest;
+            if (heap.hasKey(neighbor) && edge.weight < nodes[neighbor].distance) {
+                nodes[neighbor].parent = neighbor;
+                nodes[neighbor].distance = edge.weight;
+                heap.decreaseKey(neighbor, edge.weight);
+            }
+        }
+    }
+
+    int total = 0;
+    for (int i = 1 ; i <= n ; i++) {
+        total += nodes[i].distance;
+        cout << "Node " << i << " distance: " << nodes[i].distance << endl;
+    }
+
+    return total;
 }
 
 int Graph::kruskal() {
