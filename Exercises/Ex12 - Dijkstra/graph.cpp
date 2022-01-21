0cf1afd7c07718caa707e7c983e0214f733907a5
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2022-01-21 11:39:08 +0000

    Exercise 1 a

--- Exercises/Ex12 - Dijkstra/graph.cpp
+++ Exercises/Ex12 - Dijkstra/graph.cpp
@@ -1,28 +1,44 @@
-// AED 2021/2022 - Aula Pratica 11
-// Pedro Ribeiro (DCC/FCUP) [17/01/2022]
+// Created on January, 2022
+// @author: Fábio Araújo de Sá
 
 #include "graph.h"
 #include <climits>
 
-// Constructor: nr nodes and direction (default: undirected)
 Graph::Graph(int num, bool dir) : n(num), hasDir(dir), nodes(num+1) {
 }
 
-// Add edge from source to destination with a certain weight
 void Graph::addEdge(int src, int dest, int weight) {
     if (src<1 || src>n || dest<1 || dest>n) return;
     nodes[src].adj.push_back({dest, weight});
     if (!hasDir) nodes[dest].adj.push_back({src, weight});
 }
 
+void Graph::dijkstra(int s) {
+    MinHeap<int, int> q(n, -1);
+    for (int v=1; v<=n; v++) {
+        nodes[v].dist = INF;
+        q.insert(v, INF);
+        nodes[v].visited = false;
+    }
+    nodes[s].dist = 0;
+    q.decreaseKey(s, 0);
+    nodes[s].pred = s;
+    while (q.getSize()>0) {
+        int u = q.removeMin();
+        // cout << "Node " << u << " with dist = " << nodes[u].dist << endl;
+        nodes[u].visited = true;
+        for (auto e : nodes[u].adj) {
+            int v = e.dest;
+            int w = e.weight;
+            if (!nodes[v].visited && nodes[u].dist + w < nodes[v].dist) {
+                nodes[v].dist = nodes[u].dist + w;
+                q.decreaseKey(v, nodes[v].dist);
+                nodes[v].pred = u;
+            }
+        }
+    }
+}
 
-// ----------------------------------------------------------
-// 1) Algoritmo de Dijkstra e caminhos mais curtos
-// ----------------------------------------------------------
-
-// ..............................
-// a) Distância entre dois nós
-// TODO
 int Graph::dijkstra_distance(int a, int b) {
     return -1;
 }
