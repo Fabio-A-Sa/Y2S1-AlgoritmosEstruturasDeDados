8dc97b0922fed7e86645dc6c3dee40e7bc3597be
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-02-10 22:54:12 +0000

    fts

--- Notes/tempScript.cpp
+++ Notes/tempScript.cpp
@@ -2,15 +2,32 @@
 
 #include <iostream>
 #include <queue>
+#include <ctime>
 using namespace std;
 
 void showContent(priority_queue<int> numbers) {
 
+    while (!numbers.empty()) {
+        cout << numbers.top() << ", ";
+        numbers.pop();
+    }
+    cout << endl;
+}
 
+void fillContent(priority_queue<int> &numbers, int size) {
+
+    while (numbers.size() < size) {
+        int attemp = rand() % 20;
+        cout << "Inserindo n = " << attemp << endl;
+        numbers.push(attemp);
+    }
 }
 
 int main () {
 
+    srand(time(NULL));
+    priority_queue<int> numbers;
+    fillContent(numbers, 10);
 
     return 0;
 }
